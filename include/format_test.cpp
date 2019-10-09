57a5570aebcbddea19f7175ac3e612a0034ab8f0
blaise <blaise.lengrand@gmail.com>
2019-10-09 22:19:32 +0200

    Update implementation format (wip)

--- include/format_test.cpp
+++ include/format_test.cpp
@@ -5,19 +5,19 @@ TEST(Format, Integer)
 {
 	bzd::String<10> str;
 
-	bzd::format::toString(str, 12);
+	bzd::toString(str, 12);
 	EXPECT_EQ(str.size(), 2);
     EXPECT_STREQ(str.data(), "12");
 
-	bzd::format::toString(str, -426);
+	bzd::toString(str, -426);
 	EXPECT_EQ(str.size(), 4);
     EXPECT_STREQ(str.data(), "-426");
 
-	bzd::format::toString(str, 0);
+	bzd::toString(str, 0);
 	EXPECT_EQ(str.size(), 1);
     EXPECT_STREQ(str.data(), "0");
 
-	bzd::format::toString(str, static_cast<long long int>(1234567890));
+	bzd::toString(str, static_cast<long long int>(1234567890));
 	EXPECT_EQ(str.size(), 10);
     EXPECT_STREQ(str.data(), "1234567890");
 }
@@ -26,20 +26,20 @@ TEST(Format, Overflow)
 {
 	{
 		bzd::String<0> str;
-		bzd::format::toString(str, 12);
+		bzd::toString(str, 12);
 		EXPECT_EQ(str.size(), 0);
-		bzd::format::toString(str, -12);
+		bzd::toString(str, -12);
 		EXPECT_EQ(str.size(), 0);
 	}
 
 	{
 		bzd::String<1> str;
-		bzd::format::toString(str, 12);
+		bzd::toString(str, 12);
 		EXPECT_EQ(str.size(), 1);
-    	EXPECT_STREQ(str.data(), "2");
-		bzd::format::toString(str, -12);
+    	EXPECT_STREQ(str.data(), "1");
+		bzd::toString(str, -12);
 		EXPECT_EQ(str.size(), 1);
-    	EXPECT_STREQ(str.data(), "2");
+    	EXPECT_STREQ(str.data(), "-");
 	}
 }
 
@@ -47,20 +47,23 @@ TEST(Format, StringFormat)
 {
 	bzd::String<32> str;
 
-	bzd::format::toString(str, "Hello");
+	bzd::format(str, "Hello");
 	EXPECT_STREQ(str.data(), "Hello");
 
-	bzd::format::toString(str, "Hello {i}", 42);
+	bzd::format(str, "Hello {:i}", 42);
 	EXPECT_STREQ(str.data(), "Hello 42");
 
-	bzd::format::toString(str, "Hello {{");
+	bzd::format(str, "Hello {{");
 	EXPECT_STREQ(str.data(), "Hello {");
 
-	bzd::format::toString(str, "{{");
+	bzd::format(str, "{{");
 	EXPECT_STREQ(str.data(), "{");
 
-	bzd::format::toString(str, "Hello {{ {i", 42);
+	bzd::format(str, "Hello {{ {:i}", 42);
 	EXPECT_STREQ(str.data(), "Hello { 42");
+
+	bzd::format(str, "Hello {:i} {:i} {:i}", 1, 2, 3);
+	EXPECT_STREQ(str.data(), "Hello 1 2 3");
 
 	// to test overflows
 }
---
+++ include/to_string.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "include/container/string.h"
+#include "include/container/iostream.h"
+#include "include/container/string_stream.h"
+#include "include/type_traits/fundamental.h"
+
+namespace bzd
+{
+	namespace impl
+	{
+		namespace to_string
+		{
+			template <class T, SizeType Base = 10>
+			constexpr void integer(interface::String& str, const T n)
+			{
+				constexpr const char* const digits = "0123456789abcdef";
+				T number = (n < 0) ? -n : n;
+				auto index = str.capacity();
+
+				if (index)
+				{
+					do
+					{
+						const int digit = digits[static_cast<int>(number % Base)];
+						number /= Base;
+						str[--index] = static_cast<char>(digit);
+					} while (number && index);
+
+					if (n < 0 && index)
+					{
+						str[--index] = '-';
+					}
+
+					memmove(str.data(), &str[index], str.capacity() - index);
+				}
+				str.resize(str.capacity() - index);
+			}
+		}
+	}
+
+	template <class T, typename bzd::typeTraits::enableIf<typeTraits::isIntegral<T>::value, T>::type* = nullptr>
+	constexpr void toString(bzd::OStream& stream, const T data)
+	{
+		bzd::String<40> buffer; // 40 is a the length of the max value of an 128-bit in base 10
+		bzd::impl::to_string::integer(buffer, data);
+		stream.write(Span<const char>(buffer.data(), buffer.size()));
+	}
+
+	template <class T>
+	constexpr void toString(bzd::interface::String& str, T&& data)
+	{
+		str.clear();
+		bzd::interface::StringStream sstream(str);
+		toString(static_cast<bzd::OStream&>(sstream), bzd::forward<T>(data));
+	}
+}
