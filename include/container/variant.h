5ad4fde31771df598c538c87dd8de4164226cfa1
Blaise <blaise.lengrand@gmail.com>
2019-10-20 18:34:45 +0200

    Adding variant (still wip)

--- include/container/variant.h
+++ include/container/variant.h
@@ -8,74 +8,152 @@
 
 namespace bzd
 {
-	template <class... Ts>
-	class Variant
+	namespace impl
 	{
-	private:
-    	using Self = Variant<Ts...>;
-		// Metaprogramming list type
-		using TypeList = bzd::tmp::TypeList<Ts...>;
+		template <class StorageType, class... Ts>
+		class Variant
+		{
+		protected:
+			using Self = Variant<StorageType, Ts...>;
+			// Metaprogramming list type
+			using TypeList = bzd::tmp::TypeList<Ts...>;
+			// Choose the Nth element out of the list
+			template <SizeType N>
+			using ChooseNth = typename TypeList::template ChooseNth<N>;
+			// Checks if the type T is contained into the list
+			template <class T>
+			using Contains = typename TypeList::template Contains<T>;
+			// Search for T in the list
+			template <class T>
+			using Find = typename TypeList::template Find<T>;
+
+			//using StorageType = bzd::tmp::Union<Ts...>;
+
+			// Helper
+			template <SizeType N, SizeType Max, template<class> class F, class... Args>
+			struct HelperT
+			{
+				static auto call(const SizeType index, Args&&... args)
+				{
+					using T = ChooseNth<N>;
+					if (index == N)
+					{
+						return F<T>::call(bzd::forward<Args>(args)...);
+					} 
+					return HelperT<N + 1, Max, F, Args...>::call(index, bzd::forward<Args>(args)...);
+				}
+			};
+
+			template <SizeType N, template<class> class F, class... Args>
+			struct HelperT<N, N, F, Args...>
+			{
+				static auto call(const SizeType index, Args&&... args)
+				{
+					using T = ChooseNth<N>;
+					bzd::assertTrue(index == N, "Inconsistent variant state, should never happen");
+					return F<T>::call(bzd::forward<Args>(args)...);
+				}
+			};
+
+			template <template<class> class F, class... Args>
+			using Helper = HelperT<0, sizeof...(Ts) - 1, F, Args...>;
+
+		public:
+			/**
+			 * Default constructor
+			 */
+			constexpr Variant() = default;
+
+			/**
+			 * Value constructor
+			 */
+			template <class T, typename bzd::typeTraits::enableIf<Contains<T>::value>::type* = nullptr>
+			constexpr Variant(T&& value)
+					: id_(Find<T>::value), data_(bzd::forward<T>(value))
+			{
+			}
+
+			template <class T>
+			constexpr bool is() const noexcept
+			{
+				return (id_ != -1 && id_ == Find<T>::value);
+			}
+/*
+			template <class T>
+			constexpr bzd::Expected<const T&, bool> get() const
+			{
+				if (!is<T>())
+				{
+					return bzd::makeUnexpected(false);
+				}
+				return *reinterpret_cast<T*>(&data_);
+			}
+
+			template <class T>
+			constexpr bzd::Expected<T&, bool> get()
+			{
+				if (!is<T>())
+				{
+					return bzd::makeUnexpected(false);
+				}
+				return *reinterpret_cast<T*>(&data_);
+			}
+*/
+		protected:
+			int id_ = -1;
+			StorageType data_ = {};
+		};
+	}
+
+	template <class... Ts>
+	class VariantConstexpr : public bzd::impl::Variant<bzd::tmp::UnionConstexpr<Ts...>, Ts...>
+	{
+	};
+
+	template <class... Ts>
+	class Variant : public bzd::impl::Variant<bzd::tmp::Union<Ts...>, Ts...>
+	{
+	protected:
+		using Self = Variant<Ts...>;
+		using Parent = bzd::impl::Variant<bzd::tmp::Union<Ts...>, Ts...>;
+
 		// Choose the Nth element out of the list
 		template <SizeType N>
-		using ChooseNth = typename TypeList::template ChooseNth<N>;
+		using ChooseNth = typename Parent::template ChooseNth<N>;
 		// Checks if the type T is contained into the list
 		template <class T>
-		using Contains = typename TypeList::template Contains<T>;
+		using Contains = typename Parent::template Contains<T>;
 		// Search for T in the list
 		template <class T>
-		using Find = typename TypeList::template Find<T>;
+		using Find = typename Parent::template Find<T>;
+		// Helper class
+		template <template<class> class F, class... Args>
+		using Helper = typename Parent::template Helper<F, Args...>;
+
+		// Data types
+		using Parent::id_;
+		using Parent::data_;
+
+	protected:
 
 		// Destructor
-        template <class T>
-        struct VariantDestructor
+		template <class T>
+		struct VariantDestructor
 		{
-            static void call(Self* self)
+			static void call(Self* self)
 			{
-				reinterpret_cast<T*>(&(self->data_))->~T();
-            }
-        };
-
-		// Helper
-        template <SizeType N, SizeType Max, template<class> class F, class... Args>
-        struct HelperT
-		{
-            static auto call(const SizeType index, Args&&... args)
-			{
-                using T = ChooseNth<N>;
-                if (index == N)
-				{
-                    return F<T>::call(bzd::forward<Args>(args)...);
-                } 
-				return HelperT<N + 1, Max, F, Args...>::call(index, bzd::forward<Args>(args)...);
-            }
-        };
-
-        template <SizeType N, template<class> class F, class... Args>
-        struct HelperT<N, N, F, Args...>
-		{
-            static auto call(const SizeType index, Args&&... args)
-			{
-                using T = ChooseNth<N>;
-				bzd::assertTrue(index == N, "Inconsistent variant state, should never happen");
-                return F<T>::call(bzd::forward<Args>(args)...);
-            }
-        };
-
-    	template <template<class> class F, class... Args>
-        using Helper = HelperT<0, sizeof...(Ts) - 1, F, Args...>;
-
+				reinterpret_cast<T*>(&(self->data_.template get<T>()))->~T();
+			}
+		};
 		using Destructor = Helper<VariantDestructor, Self*>;
 
-		using StorageType = typename bzd::alignedStorage<bzd::tmp::variadicMax(sizeof(Ts)...), bzd::tmp::variadicMax(alignof(Ts)...)>::type;
-
 	private:
-
 		template <class T, class... Args, typename bzd::typeTraits::enableIf<Contains<T>::value>::type* = nullptr>
 		constexpr void construct(Args&&... args)
 		{
 			static_assert(Find<T>::value != -1, "Inconsistent variant state, should never happen");
 			// Using inplace operator new
-			::new (&data_) T(bzd::forward<Args>(args)...);
+			::new (&(data_.template get<T>())) T(bzd::forward<Args>(args)...);
 			// Sets the ID only if the constructor succeeded
 			id_ = Find<T>::value;
 		}
@@ -89,14 +167,18 @@ namespace bzd
 		}
 
 	public:
-		constexpr Variant() = default;
+		// Forward constructor to the main class
+		template <class... Args>
+		Variant(Args&&... args) : Parent::Variant(bzd::forward<Args>(args)...) {}
+
+	/*	constexpr Variant() = default;
 
 		template <class T, typename bzd::typeTraits::enableIf<Contains<T>::value>::type* = nullptr>
 		constexpr Variant(T&& value)
 		{
 			construct<T>(bzd::forward<T>(value));
 		}
-
+*/
 		template <class T, class... Args, typename bzd::typeTraits::enableIf<Contains<T>::value>::type* = nullptr>
 		constexpr void emplace(Args&&... args)
 		{
@@ -108,35 +190,5 @@ namespace bzd
 		{
 			destructIfNeeded();
 		}
-
-		template <class T>
-		constexpr bool is() const noexcept
-		{
-			return (id_ != -1 && id_ == Find<T>::value);
-		}
-
-		template <class T>
-		constexpr bzd::Expected<const T&, bool> get() const
-		{
-			if (!is<T>())
-			{
-				return bzd::makeUnexpected(false);
-			}
-			return *reinterpret_cast<T*>(&data_);
-		}
-
-		template <class T>
-		constexpr bzd::Expected<T&, bool> get()
-		{
-			if (!is<T>())
-			{
-				return bzd::makeUnexpected(false);
-			}
-			return *reinterpret_cast<T*>(&data_);
-		}
-
-	protected:
-		int id_ = -1;
-		StorageType data_ = {};
 	};
 }
--- include/template_metaprogramming_test.cpp
+++ include/template_metaprogramming_test.cpp
@@ -47,3 +47,30 @@ TEST(TMP, Find)
 	const int c = bzd::tmp::Find<bool, int, double>::value;
 	EXPECT_EQ(c, -1);
 }
+
+TEST(TMP, Union)
+{
+	bzd::tmp::Union<int, bool, float> test;
+
+	test.get<int>() = 21;
+	EXPECT_EQ(test.get<int>(), 21);
+	test.get<bool>() = true;
+	EXPECT_TRUE(test.get<bool>());
+	test.get<float>() = 3.435;
+	EXPECT_NEAR(test.get<float>(), 3.435, 0.0001);
+
+	// Constructor
+	bzd::tmp::Union<int, bool, float> testInt(static_cast<int>(42));
+	EXPECT_EQ(testInt.get<int>(), 42);
+
+	const bzd::tmp::Union<int, bool, float> constTest(static_cast<bool>(true));
+	EXPECT_TRUE(constTest.get<bool>());
+
+	constexpr bzd::tmp::UnionConstexpr<int, bool, float> constexprTest(static_cast<float>(32.5));
+	EXPECT_NEAR(constexprTest.get<float>(), 32.5, 0.001);
+
+	// Constexpr Copy Constructor
+	constexpr auto constexprTest2(constexprTest);
+	constexpr auto constexprTest3 = constexprTest2;
+	EXPECT_NEAR(constexprTest3.get<float>(), 32.5, 0.001);
+}
