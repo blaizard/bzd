e660fca01596f5b4ca7ece815105bba35bfdd9ee
blaise <blaise.lengrand@gmail.com>
2019-10-28 23:49:18 +0100

    Update with some attempt of md

--- tools/documentation/render.py
+++ tools/documentation/render.py
@@ -6,20 +6,54 @@ import re
 class Render:
 
 	def __init__(self):
-		self.groups = {}
+		pass
 
-	def printMemberDefinition(self, member):
-		formatStr = "{name}"
-		if member["kind"] == "struct":
-			formatStr = "{template} struct {name}"
-		elif member["kind"] == "class":
-			formatStr = "{template} class {name}"
-		elif member["kind"] == "typedef":
-			formatStr = "{template} typedef {name}"
-		elif member["kind"] == "variable":
-			formatStr = "{template} {static} {const} {type} {name}"
-		elif member["kind"] == "function":
-			formatStr = "{template} {static} {virtual} {explicit} {type} {name}({args}) {const}"
+	def printMemberDefinition(self, member, formatStr = "{template} {pre} {type} {name} {post}"):
+
+		formatRules = {
+			"struct": {
+				"template": "{template}",
+				"pre": "",
+				"post": "",
+				"type": "struct",
+				"name": "{name}"
+			},
+			"class": {
+				"template": "{template}",
+				"pre": "",
+				"post": "",
+				"type": "class",
+				"name": "{name}"
+			},
+			"typedef": {
+				"template": "{template}",
+				"pre": "",
+				"post": "",
+				"type": "typedef",
+				"name": "{name}"
+			},
+			"variable": {
+				"template": "{template}",
+				"pre": "{static}",
+				"post": "",
+				"type": "{const} {type}",
+				"name": "{name}"
+			},
+			"function": {
+				"template": "{template}",
+				"pre": "{static} {virtual} {explicit}",
+				"post": "",
+				"type": "{type}",
+				"name": "{name}({args}) {const}"
+			},
+			"default": {
+				"template": "",
+				"pre": "",
+				"post": "",
+				"type": "",
+				"name": "{name}"
+			}
+		}
 
 		def printTemplate(template):
 			formatArgs = ", ".join(["{type} {name}".format(type=v["__info__"].get("type"), name=v["__info__"].get("name", "")).strip() for v in template])
@@ -28,7 +62,9 @@ class Render:
 		def printArgs(args):
 			return ", ".join(["{type} {name}".format(type=v["__info__"].get("type"), name=v["__info__"].get("name", "")).strip() for v in args])
 
-		definition = formatStr.format(name = member["name"],
+		formatRule = formatRules[member["kind"]] if member["kind"] in formatRules else formatRules["default"]
+		for key, rule in formatRule.items():
+			formatRule[key] = rule.format(name = member["name"],
 				type=member.get("type") if member.get("type", None) else "",
 				virtual="virtual" if member.get("virtual", False) else "",
 				const="const" if member.get("const", False) else "",
@@ -37,27 +73,32 @@ class Render:
 				template=printTemplate(member.get("template")) if member.get("template", False) else "",
 				args=printArgs(member.get("args")) if member.get("args", False) else "")
 
+		definition = formatStr.format(template=formatRule["template"],
+				pre=formatRule["pre"],
+				type=formatRule["type"],
+				name=formatRule["name"],
+				post=formatRule["post"])
+
 		return re.sub(' +', ' ', definition.strip())
 
-	def processMembers(self, member, namespaceList):
-		namespace = "::".join(namespaceList[:-1])
-		self.groups[namespace] = self.groups[namespace] if namespace in self.groups else []
-		self.groups[namespace].append(member)
-
-	def makeGroups(self, data, namespaceList = []):
-		for namespace, definition in data.items():
-			if namespace == "__info__":
-				self.processMembers(definition, namespaceList)
-			else:
-				namespaceList.append(namespace)
-				self.makeGroups(definition, namespaceList)
-				namespaceList.pop()
+	def createListing(self, namespace, members):
+		sorted(members, key=lambda k: k.get("kind", None))
+		prevKind = -1
+		print("")
+		print("")
+		print("# {}".format(namespace))
+		for member in members:
+			kind = member.get("kind", None)
+			if kind != prevKind:
+				print("### {}".format(kind))
+				print("|||")
+				print("|---:|:---|")
+			print("|{}|`{}`|".format(self.printMemberDefinition(member, "{type}"), self.printMemberDefinition(member, "{name}")))
+			prevKind = kind
 
 	def process(self, data):
-		self.makeGroups(data)
-
-		for namespace, members in self.groups.items():
+		for namespace, members in data.items():
 			print(namespace)
+			self.createListing(namespace, members)
 			for member in members:
 				print(" - %s" % (self.printMemberDefinition(member)))
-
