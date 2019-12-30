49ef357289031fa64860e7bbe92cfc8d8b778774
Blaise <blaise.lengrand@gmail.com>
2019-12-30 12:04:22 +0100

    Adding inheritance test

--- tests/registry_test.cpp
+++ tests/registry_test.cpp
@@ -3,10 +3,32 @@
 
 TEST(Registry, Base)
 {
-	bzd::declare::Registry<double, 3> reg_;
-	bzd::Registry<double> hello("hello", 12);
+	bzd::declare::Registry<int, 3> reg_;
+	bzd::Registry<int> hello("hello", 12);
+	bzd::Registry<int> yes("yes", -1);
+	bzd::Registry<int> me("me", 986);
 
-    EXPECT_EQ(bzd::Registry<double>::get("hello"), 12);
+    EXPECT_EQ(bzd::Registry<int>::get("hello"), 12);
+    EXPECT_EQ(bzd::Registry<int>::get("me"), 986);
+    EXPECT_EQ(bzd::Registry<int>::get("yes"), -1);
+}
+
+TEST(Registry, Inheritance)
+{
+	struct Foo
+	{
+		int a;
+	};
+
+	struct Bar : Foo
+	{
+		Bar(int a_) : Foo{a_} {}
+	};
+
+	bzd::declare::Registry<Foo, 1> reg_;
+	bzd::Registry<Foo, Bar> hello("hello", 12);
+
+    EXPECT_EQ(bzd::Registry<Foo>::get("hello").a, 12);
 }
 
 // Registry are used to store logger, communication channels
