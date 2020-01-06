7718583b51c36826117bf657f56983b4d2e4930e
blaise <blaise.lengrand@gmail.com>
2020-01-06 12:16:07 +0100

    Addign sanitize tool

--- quality_gate.sh
+++ quality_gate.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Do sanitizing actions at the beginnig
-bazel run //tools/sanitizer -- --workspace $(pwd) --action clang-format
-bazel run //tools/buildifier
-
 # Compile and test the different configurations
 bazel test ... --define=panic=throw
 bazel test ... --define=panic=throw --config=linux_x86_64_clang --platform_suffix=_clang
