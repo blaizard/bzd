49329b63d83f18f6ca2054476a15bee64a6690ed
blaise <blaise.lengrand@gmail.com>
2020-01-06 14:15:25 +0100

    Moving example bzd with format

--- example/bzd/main.cpp
+++ example/bzd/main.cpp
@@ -1,11 +1,10 @@
 #include "bzd/core/registry.h"
 #include "bzd/core/system.h"
+#include "bzd/format/format.h"
 
 int main()
 {
-	bzd::getOut().write(bzd::StringView{"Message: "});
-	bzd::getOut().write(bzd::Registry<bzd::StringView>::get("message"));
-	bzd::getOut().write(bzd::StringView{"\n"});
+	bzd::format::toString(bzd::getOut(), CONSTEXPR_STRING_VIEW("Message: {}\n"), bzd::Registry<bzd::StringView>::get("message"));
 
 	return 0;
 }
