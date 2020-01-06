78db01478fe7dffab336fe6b95c9481e54ecfa6f
blaise <blaise.lengrand@gmail.com>
2020-01-06 13:35:17 +0100

    Fixing script

--- tools/bazel.build/setup_bazel.sh
+++ tools/bazel.build/setup_bazel.sh
@@ -4,5 +4,7 @@ apt-get purge -y bazel
 
 url="https://github.com/bazelbuild/bazel/releases/download/2.0.0/bazel_2.0.0-linux-x86_64.deb"
 
-curl -L ${url} -o /tmp/${package}
-dpkg -i /tmp/${package}
+mkdir -p ./tmp.bazel.build
+curl -L ${url} -o ./tmp.bazel.build/${package}
+dpkg -i ./tmp.bazel.build/${package}
+rm -rfd ./tmp.bazel.build
