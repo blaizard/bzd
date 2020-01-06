f1ec17a3559a909d3bc1cb95075e4a9d960f0881
Blaise <blaise.lengrand@gmail.com>
2020-01-06 23:07:19 +0100

    Fixed warning

--- quality_gate.sh
+++ quality_gate.sh
@@ -15,4 +15,4 @@ bazel test ... --define=panic=throw
 bazel test ... --define=panic=throw --config=linux_x86_64_clang --platform_suffix=_clang
 
 # Use static analyzers
-bazel test ... --define=panic=throw --config=linux_x86_64_clang --config=asan --config=lsan --platform_suffix=_clang_asan_lsan
+bazel test ... --define=panic=throw --config=linux_x86_64_clang --config=sanitizer --config=asan --config=lsan --platform_suffix=_clang_asan_lsan
