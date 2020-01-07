8ce8fa974963b5ff2b8bb0eecbf1bbcfe25a11f8
blaise <blaise.lengrand@gmail.com>
2020-01-07 20:38:46 +0100

    Cleanup

--- tools/bzd/generator/parser.py
+++ tools/bzd/generator/parser.py
@@ -5,17 +5,17 @@ import yaml
 from log import Log
 
 """
-Add support for !ref tags
+Add support for !ref tag
 """
 class Ref():
 	tag = u'!ref'
 	def __init__(self, loader, node):
 		self.value = node.value
+	def __repr__(self):
+		return str(self.value)
 	@staticmethod
 	def representer(dumper, data):
 		return dumper.represent_scalar(Ref.tag, str(data))
-	def __repr__(self):
-		return str(self.value)
 
 """
 Convert the dict back to a manifest
@@ -29,16 +29,14 @@ Open a manifest file and convert it to a python dict.
 """
 def manifestToDict(path):
 
-	class BzdLoader(yaml.Loader):
-		pass
-	BzdLoader.add_constructor(Ref.tag, Ref)
+	yaml.add_constructor(Ref.tag, Ref)
 
 	try:
 		f = open(path)
 	except Exception as e:
 		Log.fatal("Unable to open manifest '{}'".format(path), e)
 	try:
-		data = yaml.load(f, Loader = BzdLoader)
+		data = yaml.load(f, Loader = yaml.Loader)
 	except Exception as e:
 		Log.fatal("Unable to parse manifest '{}'".format(path), e)
 	finally:
