72740dd84075592e569357fcc5dc3b38b5fb452b
blaise <blaise.lengrand@gmail.com>
2020-01-15 23:35:35 +0100

    Set throw as default option for test

--- quality_gate.sh
+++ quality_gate.sh
@@ -12,8 +12,8 @@ if [ "$expectedGitDiff" != "$(git diff --shortstat)" ]; then
 fi
 
 # Compile and test the different configurations
-bazel test ... --define=panic=throw
-bazel test ... --define=panic=throw --config=linux_x86_64_clang --platform_suffix=_clang
+bazel test ...
+bazel test ... --config=linux_x86_64_clang --platform_suffix=_clang
 
 # Use static analyzers
-bazel test ... --define=panic=throw --config=linux_x86_64_clang --config=sanitizer --config=asan --config=lsan --platform_suffix=_clang_asan_lsan
+bazel test ... --config=linux_x86_64_clang --config=sanitizer --config=asan --config=lsan --platform_suffix=_clang_asan_lsan
