e395aeec4de0545e403d5421ca27a521e3f1258c
Blaise <blaise.lengrand@gmail.com>
2020-02-14 16:13:21 +0100

    Adding constructor as constexpr to be evaluated while compiling

--- platform/libraries/io/digital/digital_esp32_xtensa_lx6.h
+++ platform/libraries/io/digital/digital_esp32_xtensa_lx6.h
@@ -7,7 +7,11 @@ namespace bzd { namespace io { namespace impl {
 class DigitalOutputEsp32XtensaLx6 : public bzd::io::DigitalOutput
 {
 public:
-	DigitalOutputEsp32XtensaLx6(const bzd::UInt8Type& pin);
+	constexpr DigitalOutputEsp32XtensaLx6(const bzd::UInt8Type& pin) : bzd::io::DigitalOutput(), pin_(static_cast<gpio_num_t>(pin))
+	{
+		gpio_pad_select_gpio(pin_);
+		gpio_set_direction(pin_, GPIO_MODE_OUTPUT);
+	}
 
 	bzd::SizeType write(const bzd::Span<const bzd::UInt8Type>& data) noexcept override;
 
