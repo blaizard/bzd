b63a1d3b3d612c1b6930482860e239ce84a24bc6
Blaise <blaise.lengrand@gmail.com>
2020-03-10 09:53:29 +0100

    Adding getOut

--- tests/signal_test.cpp
+++ tests/signal_test.cpp
@@ -1,6 +1,8 @@
 #include "bzd/signal.h"
 #include "cc_test/test.h"
 
+#include <iostream>
+
 TEST(Signal, Base)
 {
 	bzd::ConstBuffer buffer("HelloWorld");
@@ -11,6 +13,9 @@ TEST(Signal, Base)
 	bzd::Signal<char, 40, 8> charSignal5;
 	EXPECT_EQ(charSignal5.get(buffer), 'W');
 
-	// bzd::Signal<char, 4, 8> charSignalShift;
-	// EXPECT_EQ(charSignalShift.get(buffer), 0x00);
+	bzd::Signal<char, 4, 8> charSignalShift;
+	std::cout << "Heeee   " << std::hex << static_cast<int>('H') << std::endl;
+	std::cout << "Heeee   " << std::hex << static_cast<int>('e') << std::endl;
+	std::cout << "Heeee   " << std::hex << static_cast<int>(charSignalShift.get(buffer)) << std::endl;
+	EXPECT_EQ(charSignalShift.get(buffer), 0x54);
 }
