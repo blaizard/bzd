79e34f9e4a112238a804c9e12a07d189529a5e74
blaise <blaise.lengrand@gmail.com>
2020-04-19 00:24:59 +0200

    Adding test on python version

---
+++ toolchains/python/linux_x86_64/python_test.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+
+class PythonTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(sys.version_info, (3, 6, 8, "final", 0))
+        self.assertIn("python3_linux_x86_64/usr/bin/python3.6", sys.executable)
+
+if __name__ == '__main__': 
+    unittest.main()
