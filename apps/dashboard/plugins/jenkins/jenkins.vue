dc940791be6a78ef9cc8657d1ced8359a2cb9d48
Blaise Lengrand <blaise.lengrand@aid-driving.eu>
2020-04-24 18:12:09 +0200

    clean

--- apps/dashboard/plugins/jenkins/jenkins.vue
+++ apps/dashboard/plugins/jenkins/jenkins.vue
@@ -14,8 +14,9 @@
                     name: "Jenkins",
                     icon: "bzd-icon-jenkins",
                     form: [
-                        { type: "Input", name: "jenkins.name", caption: "Name", placeholder: "Jenkins...", width: 0.5 },
-                        { type: "Dropdown", caption: "Type", width: 0.5 },
+                        { type: "Input", name: "jenkins.url", caption: "Jenkins URL", placeholder: "http://localhost:8080", width: 1 },
+                        { type: "Input", name: "jenkins.build", caption: "Build name", width: 0.5 },
+                        { type: "Dropdown", name: "jenkins.branch", caption: "Branch", placeholder: "master", list: ["master"], edit: true, width: 0.5 },
                     ]
                 }
             }
