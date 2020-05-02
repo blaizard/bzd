650353e38fd0414bb10355b1c1e6cba8e8f4d23a
Blaise Lengrand <blaise.lengrand@aid-driving.eu>
2020-05-02 15:46:15 +0200

    Fixed eslint to use local nodejs

--- nodejs/core/crypto.js
+++ nodejs/core/crypto.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import ExceptionFactory from "./exception.js";
-
-const Exception = ExceptionFactory("crypto");
-
 export async function base64Encode(data) {
 	if (process.env.BZD_RULE === "nodejs_web") {
 		return (await import(/* webpackMode: "eager" */"./impl/crypto/window.btoa.js")).default(data);
