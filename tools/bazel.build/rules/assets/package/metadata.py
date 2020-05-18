99745a6b86e72044a816c6eb038a5481f9892f34
blaise <blaise.lengrand@gmail.com>
2020-05-18 23:22:47 +0200

    Adding config

--- tools/bazel.build/rules/assets/package/metadata.py
+++ tools/bazel.build/rules/assets/package/metadata.py
@@ -8,12 +8,15 @@ if __name__== "__main__":
 
 	parser = argparse.ArgumentParser(description = "Information generator rules.")
 	parser.add_argument("-i", "--input", nargs=2, metavar=("app", "path"), action="append", help = "Pair of application name and an information package json file, can add multiple.")
+	parser.add_argument("--config", type = str, help = "Build configuration.")
 	parser.add_argument("output", default = "package.manifest", help = "Path of the generated file.")
 
 	args = parser.parse_args()
 
 	# Merge all entries together
-	output = {}
+	output = {
+		"config": args.config
+	}
 	for item in args.input:
 		with open(item[1]) as f:
 			fragment = json.load(f)
