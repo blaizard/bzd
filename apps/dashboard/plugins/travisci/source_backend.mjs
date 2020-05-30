4b35c93f220f5e5bba13db1d78d605aebc0dd1f1
blaise <blaise.lengrand@gmail.com>
2020-05-30 15:58:15 +0200

    Fix authentication

--- apps/dashboard/plugins/travisci/source_backend.mjs
+++ apps/dashboard/plugins/travisci/source_backend.mjs
@@ -29,7 +29,7 @@ export default {
                         "Travis-API-Version": "3"
                     },
                     authentication: {
-                        type: "bearer",
+                        type: "token",
                         token: token
                     }
                 };
