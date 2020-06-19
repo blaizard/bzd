88be116ff70c198c6e524593aa9a79d74ec5b947
Blaise <blaise.lengrand@gmail.com>
2020-06-20 00:07:29 +0200

    Moved coverage to tools/bazel

--- coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Generate a coverage report
-
-#./tools/bazel coverage --config=linux_x86_64_clang "$@"
-./tools/bazel coverage "$@"
-./tools/bazel run //tools/coverage:report -- --output-directory bazel-out/coverage bazel-out/_coverage/_coverage_report.dat
-echo "Coverage report: $(pwd)/bazel-out/coverage/index.html"
