eb1190509ac1fead9c957cf1961212a6ef7e0658
Blaise <blaise.lengrand@gmail.com>
2020-06-19 23:26:55 +0200

    Adding bazelisk

--- coverage.sh
+++ coverage.sh
@@ -3,7 +3,7 @@ set -e
 
 # Generate a coverage report
 
-#bazel coverage --config=linux_x86_64_clang "$@"
-bazel coverage "$@"
-bazel run //tools/coverage:report -- --output-directory bazel-out/coverage bazel-out/_coverage/_coverage_report.dat
+#./tools/bazel coverage --config=linux_x86_64_clang "$@"
+./tools/bazel coverage "$@"
+./tools/bazel run //tools/coverage:report -- --output-directory bazel-out/coverage bazel-out/_coverage/_coverage_report.dat
 echo "Coverage report: $(pwd)/bazel-out/coverage/index.html"
