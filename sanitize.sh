b913b7168ae89c2f6c87a33c6a4f3c7c1775dfac
Blaise <blaise.lengrand@gmail.com>
2020-07-04 17:32:46 +0200

    Moved buildfier to sanitizer

--- sanitize.sh
+++ sanitize.sh
@@ -6,7 +6,6 @@ WORKSPACE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 
 # Sanitize
 ./tools/bazel run //tools/sanitizer
-./tools/bazel run //tools/buildifier -- -lint "fix" -r "$WORKSPACE_PATH"
 
 # Generate documentation
 rm -rfd "$WORKSPACE_PATH/docs/api"
---
+++ tools/sanitizer/actions/buildifier/buildifier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+import argparse
+import sys
+from bzd.utils.run import localBazelBinary
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description="Wrapper for buildifier")
+	parser.add_argument("--buildifier", dest="buildifier", default="external/buildifier/file/downloaded")
+	parser.add_argument("workspace", help="Workspace to be processed.")
+
+	args = parser.parse_args()
+
+	result = localBazelBinary(args.buildifier, args = ["-lint", "fix", "-r", args.workspace])
+	print(result.getOutput())
+
+	sys.exit(result.getReturnCode())
