3468ba2bcd2bf30f98851206e87db2de9b4de6f1
Blaise <blaise.lengrand@gmail.com>
2020-07-21 23:22:28 +0200

    Improved typing

--- python/bzd/utils/run.py
+++ python/bzd/utils/run.py
@@ -5,55 +5,56 @@ import os
 import subprocess
 import sys
 import threading
+from typing import Any, List, Dict, Optional, Tuple
 import selectors
 
 class _ExecuteResultStreamWriter:
-    def __init__(self):
-        self.output = []
+    def __init__(self) -> None:
+        self.output: List[Tuple[bool, bytes]] = []
 
-    def addStdout(self, data):
+    def addStdout(self, data: bytes) -> None:
         if data != b"":
             self.output.append((True, data))
 
-    def addStderr(self, data):
+    def addStderr(self, data: bytes) -> None:
         if data != b"":
             self.output.append((False, data))
 
 class _ExecuteResult:
-    def __init__(self, stream: _ExecuteResultStreamWriter, returncode):
+    def __init__(self, stream: _ExecuteResultStreamWriter, returncode: int) -> None:
         self.output = stream.output
         self.returncode = returncode
 
-    def getStdout(self):
+    def getStdout(self) -> str:
         return b"".join([entry[1] for entry in self.output if entry[0] == True]).decode(errors = "ignore")
 
-    def getStderr(self):
+    def getStderr(self) -> str:
         return b"".join([entry[1] for entry in self.output if entry[0] == False]).decode(errors = "ignore")
 
-    def getOutput(self):
+    def getOutput(self) -> str:
         return b"".join([entry[1] for entry in self.output]).decode(errors = "ignore")
 
-    def getReturnCode(self):
+    def getReturnCode(self) -> int:
         return self.returncode
 
 """
 Run a process locally.
 """
-def localCommand(cmds, inputs = None, ignoreFailure = False, env=None, timeoutS = 60):
+def localCommand(cmds: List[str], inputs: bytes = b"", ignoreFailure: bool = False, env: Optional[Dict[str, str]] = None, timeoutS: float = 60.) -> _ExecuteResult:
 
     sel = selectors.DefaultSelector()
     stream = _ExecuteResultStreamWriter()
     proc = subprocess.Popen(cmds, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
     timer = threading.Timer(timeoutS, proc.kill)
-    sel.register(proc.stdout, selectors.EVENT_READ)
-    sel.register(proc.stderr, selectors.EVENT_READ)
+    sel.register(proc.stdout, events = selectors.EVENT_READ) # type: ignore
+    sel.register(proc.stderr, events = selectors.EVENT_READ) # type: ignore
 
     try:
         isRunning = True
         timer.start()
         while isRunning and timer.is_alive():
             for key, _ in sel.select():
-                data = key.fileobj.read1(128)
+                data = key.fileobj.read1(128) # type: ignore
                 if not data:
                     isRunning = False
                 (stream.addStdout if key.fileobj is proc.stdout else stream.addStderr)(data)
@@ -78,18 +79,18 @@ def localCommand(cmds, inputs = None, ignoreFailure = False, env=None, timeoutS 
 """
 Execute a python script locally.
 """
-def localPython(script, args = [], **kargs):
+def localPython(script: str, args: List[str] = [], **kargs: Any) -> _ExecuteResult:
     return localCommand([sys.executable, script] + args, **kargs)
 
 """
 Execute a bash script locally.
 """
-def localBash(script, args = [], **kargs):
+def localBash(script: bytes, args: List[str] = [], **kargs: Any) -> _ExecuteResult:
     return localCommand(["bash", "-s", "--"] + args, inputs=script, **kargs)
 
 """
 Execute a bazel binary locally. The caller must run bazel and all runfiles must be already available.
 """
-def localBazelBinary(path, args=[], env={}, **kargs):
+def localBazelBinary(path: str, args: List[str] = [], env: Dict[str, str] = {}, **kargs: Any) -> _ExecuteResult:
     env["RUNFILES_DIR"] = os.environ["RUNFILES_DIR"] if "RUNFILES_DIR" in os.environ else os.path.dirname(os.getcwd())
     return localCommand([path] + args, env=env, **kargs)
