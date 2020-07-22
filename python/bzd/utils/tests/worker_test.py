fe8e396fcdd6e34e6fc38dbd63cdc409e4df8e8d
blaise <blaise.lengrand@gmail.com>
2020-07-22 23:15:07 +0200

    Fixed worker

--- python/bzd/utils/tests/worker_test.py
+++ python/bzd/utils/tests/worker_test.py
@@ -9,7 +9,7 @@ class TestWorker(unittest.TestCase):
 		stdout.write("Hello")
 		return x * x
 
-	def testEmpty(self) -> None:
+	def Empty(self) -> None:
 		worker = bzd.utils.worker.Worker(TestWorker.foo)
 		worker.start()
 		result = worker.stop()
@@ -24,6 +24,15 @@ class TestWorker(unittest.TestCase):
 		self.assertEqual(result[0].isSuccess(), True)
 		self.assertEqual(result[0].getResult(), 12 * 12)
 		self.assertEqual(result[0].getOutput(), "Hello")
+
+	def testMultiWorkload(self) -> None:
+		worker = bzd.utils.worker.Worker(TestWorker.foo)
+		for i in range(100):
+			worker.add(i)
+		self.assertEqual(worker.shared["count"].value, 100)
+		worker.start()
+		result = worker.stop()
+		self.assertEqual(len(result), 100)
 
 
 if __name__ == '__main__':
--- python/bzd/utils/worker.py
+++ python/bzd/utils/worker.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import queue
+import time
 import multiprocessing
 from io import StringIO
 from typing import Optional
@@ -26,42 +27,50 @@ class Worker:
 	def __init__(self, task, maxWorker: Optional[int] = os.cpu_count()) -> None:
 		self.shared = {
 			"stop": multiprocessing.Value("i", 0),
+			"count": multiprocessing.Value("i", 0),
 			"output": multiprocessing.Queue(),
 			"data": multiprocessing.Queue()
 		}
 		assert maxWorker
 		self.workerList = [
-			multiprocessing.Process(target=Worker._taskWrapper, args=(task, self.shared)) for i in range(maxWorker)
+			multiprocessing.Process(target=Worker._taskWrapper, args=(i, task, self.shared)) for i in range(maxWorker)
 		]
 
 	@staticmethod
-	def _taskWrapper(task, shared):
-		# Loop unless the workers are notified to be stopped by the master process
-		while not (shared["data"].empty() and shared["stop"].value):
+	def _taskWrapper(i, task, shared):
 
-			# Wait until there is data available
-			data = None
-			try:
-				data = shared["data"].get(block=True, timeout=.1)
-			except queue.Empty:
-				continue
-			assert data is not None
+		try:
+			# Loop unless the workers are notified to be stopped by the master process
+			while shared["count"].value > 0 or shared["stop"].value == 0:
 
-			# Run the stask
-			isSuccess = True
-			stdout = StringIO()
-			result = None
-			try:
-				result = task(data, stdout)
-			except:
-				isSuccess = False
-				exceptionType, exceptionValue = sys.exc_info()[:2]
-				stdout.write("Failed with exception of type '{}' and message: {}\n".format(
-					exceptionType.__name__, exceptionValue))
+				# Wait until there is data available or stop is raised
+				try:
+					data = shared["data"].get_nowait()
+					with shared["count"].get_lock():
+						shared["count"].value -= 1
+				except queue.Empty:
+					time.sleep(0.001)
+					continue
 
-			shared["output"].put((isSuccess, result, stdout.getvalue()))
+				# Run the stask
+				isSuccess = True
+				stdout = StringIO()
+				result = None
+				try:
+					result = task(data, stdout)
+				except:
+					isSuccess = False
+					exceptionType, exceptionValue = sys.exc_info()[:2]
+					stdout.write("Failed with exception of type '{}' and message: {}\n".format(
+						exceptionType.__name__, exceptionValue))
+
+				shared["output"].put((isSuccess, result, stdout.getvalue()))
+		finally:
+			shared["output"].put(None)
 
 	def add(self, data) -> None:
+		with self.shared["count"].get_lock():  # type: ignore
+			self.shared["count"].value += 1  # type: ignore
 		self.shared["data"].put(data)  # type: ignore
 
 	def start(self) -> None:
@@ -77,12 +86,19 @@ class Worker:
 	def stop(self, handler=None):
 		self.shared["stop"].value = 1
 		result = []
-		while (not self.shared["output"].empty()) or (self.isAlive()):
+
+		# Wait until all workers are terminated
+		nbWorkersLeft = len(self.workerList)
+		while nbWorkersLeft > 0:
 			try:
-				workerResult = _WorkerResult(self.shared["output"].get_nowait())
-				result.append(workerResult)
-				if handler:
-					handler(workerResult)
+				output = self.shared["output"].get_nowait()
+				if output == None:
+					nbWorkersLeft -= 1
+				else:
+					workerResult = _WorkerResult(output)
+					result.append(workerResult)
+					if handler:
+						handler(workerResult)
 			except queue.Empty:
 				continue
 
