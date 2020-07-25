a7b3999e62145690dcd32000e6fe4810562ada06
Blaise <blaise.lengrand@gmail.com>
2020-07-25 16:06:34 +0200

    sanitize

--- python/bzd/yaml/emitter.py
+++ python/bzd/yaml/emitter.py
@@ -238,14 +238,12 @@ class Emitter:
 			if isinstance(self.event, ScalarEvent):
 				self.expect_scalar()
 			elif isinstance(self.event, SequenceStartEvent):
-				if self.flow_level or self.canonical or self.event.flow_style   \
-                                                                                                                                                                                    or self.check_empty_sequence():
+				if self.flow_level or self.canonical or self.event.flow_style or self.check_empty_sequence():
 					self.expect_flow_sequence()
 				else:
 					self.expect_block_sequence()
 			elif isinstance(self.event, MappingStartEvent):
-				if self.flow_level or self.canonical or self.event.flow_style   \
-                                                                                                                                                                                    or self.check_empty_mapping():
+				if self.flow_level or self.canonical or self.event.flow_style or self.check_empty_mapping():
 					self.expect_flow_mapping()
 				else:
 					self.expect_block_mapping()
@@ -435,8 +433,7 @@ class Emitter:
 			if self.prepared_anchor is None:
 				self.prepared_anchor = self.prepare_anchor(self.event.anchor)
 			length += len(self.prepared_anchor)
-		if isinstance(self.event, (ScalarEvent, CollectionStartEvent))  \
-                                                                                              and self.event.tag is not None:
+		if isinstance(self.event, (ScalarEvent, CollectionStartEvent)) and self.event.tag is not None:
 			if self.prepared_tag is None:
 				self.prepared_tag = self.prepare_tag(self.event.tag)
 			length += len(self.prepared_tag)
@@ -538,8 +535,7 @@ class Emitter:
 		if handle[0] != '!' or handle[-1] != '!':
 			raise EmitterError("tag handle must start and end with '!': %r" % handle)
 		for ch in handle[1:-1]:
-			if not ('0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z'    \
-                                                                                                                                         or ch in '-_'):
+			if not ('0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' or ch in '-_'):
 				raise EmitterError("invalid character %r in the tag handle: %r" % (ch, handle))
 		return handle
 
@@ -552,8 +548,7 @@ class Emitter:
 			end = 1
 		while end < len(prefix):
 			ch = prefix[end]
-			if '0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' \
-                                                                                                                                         or ch in '-;/?!:@&=+$,_.~*\'()[]':
+			if '0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' or ch in '-;/?!:@&=+$,_.~*\'()[]':
 				end += 1
 			else:
 				if start < end:
@@ -575,17 +570,15 @@ class Emitter:
 		suffix = tag
 		prefixes = sorted(self.tag_prefixes.keys())
 		for prefix in prefixes:
-			if tag.startswith(prefix)   \
-                                                                                                                                         and (prefix == '!' or len(prefix) < len(tag)):
+			if tag.startswith(prefix) and (prefix == '!' or len(prefix) < len(tag)):
 				handle = self.tag_prefixes[prefix]
 				suffix = tag[len(prefix):]
 		chunks = []
 		start = end = 0
 		while end < len(suffix):
 			ch = suffix[end]
-			if '0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' \
-                                                                                                                                         or ch in '-;/?:@&=+$,_.~*\'()[]'   \
-                                                                                                                                         or (ch == '!' and handle != '!'):
+			if '0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' or ch in '-;/?:@&=+$,_.~*\'()[]' or (ch == '!'
+				and handle != '!'):
 				end += 1
 			else:
 				if start < end:
@@ -606,8 +599,7 @@ class Emitter:
 		if not anchor:
 			raise EmitterError("anchor must not be empty")
 		for ch in anchor:
-			if not ('0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z'    \
-                                                                                                                                         or ch in '-_'):
+			if not ('0' <= ch <= '9' or 'A' <= ch <= 'Z' or 'a' <= ch <= 'z' or ch in '-_'):
 				raise EmitterError("invalid character %r in the anchor: %r" % (ch, anchor))
 		return anchor
 
@@ -747,8 +739,7 @@ class Emitter:
 		# Spaces followed by breaks, as well as special character are only
 		# allowed for double quoted scalars.
 		if space_break or special_characters:
-			allow_flow_plain = allow_block_plain =  \
-                                                                                                                                 allow_single_quoted = allow_block = False
+			allow_flow_plain = allow_block_plain = allow_single_quoted = allow_block = False
 
 		# Although the plain scalar writer supports breaks, we never emit
 		# multiline plain scalars.
@@ -801,8 +792,7 @@ class Emitter:
 
 	def write_indent(self):
 		indent = self.indent or 0
-		if not self.indention or self.column > indent   \
-                                                                                              or (self.column == indent and not self.whitespace):
+		if not self.indention or self.column > indent or (self.column == indent and not self.whitespace):
 			self.write_line_break()
 		if self.column < indent:
 			self.whitespace = True
@@ -850,8 +840,7 @@ class Emitter:
 				ch = text[end]
 			if spaces:
 				if ch is None or ch != ' ':
-					if start+1 == end and self.column > self.best_width and split   \
-                                                                                                                                                                                                                               and start != 0 and end != len(text):
+					if start + 1 == end and self.column > self.best_width and split and start != 0 and end != len(text):
 						self.write_indent()
 					else:
 						data = text[start:end]
@@ -918,11 +907,8 @@ class Emitter:
 			ch = None
 			if end < len(text):
 				ch = text[end]
-			if ch is None or ch in '"\\\x85\u2028\u2029\uFEFF' \
-                                                                                                                                         or not ('\x20' <= ch <= '\x7E'
-				or (self.allow_unicode
-				and ('\xA0' <= ch <= '\uD7FF'
-				or '\uE000' <= ch <= '\uFFFD'))):
+			if ch is None or ch in '"\\\x85\u2028\u2029\uFEFF' or not ('\x20' <= ch <= '\x7E' or (self.allow_unicode and
+				('\xA0' <= ch <= '\uD7FF' or '\uE000' <= ch <= '\uFFFD'))):
 				if start < end:
 					data = text[start:end]
 					self.column += len(data)
@@ -944,8 +930,8 @@ class Emitter:
 						data = data.encode(self.encoding)
 					self.stream.write(data)
 					start = end + 1
-			if 0 < end < len(text)-1 and (ch == ' ' or start >= end)    \
-                                                                                                                                         and self.column+(end-start) > self.best_width and split:
+			if 0 < end < len(text) - 1 and (ch == ' '
+				or start >= end) and self.column + (end - start) > self.best_width and split:
 				data = text[start:end] + '\\'
 				if start < end:
 					start = end
@@ -992,8 +978,7 @@ class Emitter:
 				ch = text[end]
 			if breaks:
 				if ch is None or ch not in '\n\x85\u2028\u2029':
-					if not leading_space and ch is not None and ch != ' '   \
-                                                                                                                                                                                                                               and text[start] == '\n':
+					if not leading_space and ch is not None and ch != ' ' and text[start] == '\n':
 						self.write_line_break()
 					leading_space = (ch == ' ')
 					for br in text[start:end]:
--- python/bzd/yaml/scanner.py
+++ python/bzd/yaml/scanner.py
@@ -1256,10 +1256,9 @@ class Scanner:
 				break
 			while True:
 				ch = self.peek(length)
-				if ch in '\0 \t\r\n\x85\u2028\u2029'                                                          or (ch == ':' and
-					self.peek(length+1) in '\0 \t\r\n\x85\u2028\u2029'
-					+ (u',[]{}' if self.flow_level else u''))\
-                                                                                                                                                                                    or (self.flow_level and ch in ',?[]{}'):
+				if ch in '\0 \t\r\n\x85\u2028\u2029' or (ch == ':'
+					and self.peek(length + 1) in '\0 \t\r\n\x85\u2028\u2029' +
+					(u',[]{}' if self.flow_level else u'')) or (self.flow_level and ch in ',?[]{}'):
 					break
 				length += 1
 			if length == 0:
@@ -1270,8 +1269,7 @@ class Scanner:
 			self.forward(length)
 			end_mark = self.get_mark()
 			spaces = self.scan_plain_spaces(indent, start_mark)
-			if not spaces or self.peek() == '#' \
-                                                                                                                                         or (not self.flow_level and self.column < indent):
+			if not spaces or self.peek() == '#' or (not self.flow_level and self.column < indent):
 				break
 		return ScalarToken(''.join(chunks), True, start_mark, end_mark)
 
