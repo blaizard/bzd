2c6be2ac04292e15e71bd8e46b85a578923a6f5d
Blaise <blaise.lengrand@gmail.com>
2020-08-18 09:15:42 +0200

    Adding Validation in plugins

--- apps/artifacts/plugins/docker/frontend.mjs
+++ apps/artifacts/plugins/docker/frontend.mjs
@@ -11,16 +11,16 @@ export default {
 				gcr: "Google Container Registry"
 			}
 		},
-		{ type: "Input", name: "docker.url", caption: "Registry URL", condition: (value) => value["docker.type"] == "v2" },
+		{ type: "Input", name: "docker.url", caption: "Registry URL", condition: { "docker.type": "values(v2)" } },
 		{
 			type: "Dropdown",
 			name: "docker.service",
 			editable: true,
 			caption: "Service",
 			list: ["gcr.io", "us.gcr.io", "eu.gcr.io", "asia.gcr.io"],
-			condition: (value) => value["docker.type"] == "gcr"
+			condition: { "docker.type": "values(gcr)" }
 		},
-		{ type: "Textarea", name: "docker.key", caption: "JSON key", condition: (value) => value["docker.type"] == "gcr" }
+		{ type: "Textarea", name: "docker.key", caption: "JSON key", condition: { "docker.type": "values(gcr)" } }
 	],
 	visualization: () => import("../table.vue")
 };
