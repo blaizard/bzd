834e84af9404df145362a54bfd27d44d9c1c0312
Blaise <blaise.lengrand@gmail.com>
2020-08-28 22:14:08 +0200

    Removed test data

--- apps/artifacts/backend/app.mjs
+++ apps/artifacts/backend/app.mjs
@@ -9,7 +9,7 @@ import LogFactory from "bzd/core/log.mjs";
 import ExceptionFactory from "bzd/core/exception.mjs";
 import Cache from "bzd/core/cache.mjs";
 import { CollectionPaging } from "bzd/db/utils.mjs";
-import Filesystem from "bzd/core/filesystem.mjs";
+//import Filesystem from "bzd/core/filesystem.mjs";
 import Services from "./services.mjs";
 import Plugins from "../plugins/backend.mjs";
 import Permissions from "bzd/db/storage/permissions.mjs";
@@ -46,7 +46,7 @@ Commander.version("1.0.0", "-v, --version")
 	let keyValueStore = await KeyValueStoreDisk.make(Path.join(PATH_DATA, "db"));
 
 	// Test data
-
+/*
 	await keyValueStore.set("volume", "disk", {
 		type: "fs",
 		"fs.root": "/"
@@ -68,7 +68,7 @@ Commander.version("1.0.0", "-v, --version")
 		"docker.proxy.url": "http://127.0.0.1:5050",
 		"docker.proxy.port": 5051
 	});
-
+*/
 	// Set the cache
 	let cache = new Cache();
 
