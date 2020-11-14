18be325ce5910ed01f38de6edc64220bc7260035
Blaise <blaise.lengrand@gmail.com>
2020-11-14 19:54:35 +0100

    Renaming task switch to shift

--- cc/bzd/platform/stack/x86_64_system_v/stack.cc
+++ cc/bzd/platform/stack/x86_64_system_v/stack.cc
@@ -10,5 +10,5 @@ void bzd::platform::interface::StackUser::reset(const FctPtrType fct) noexcept
 	pStack[0] = reinterpret_cast<bzd::UInt64Type>(fct);
 	// MMX & x87, default MXCSR value at reset is 1F80H
 	pStack[-7] = (static_cast<bzd::UInt64Type>(0x0) << 32) | 0x1f80;
-	stack_ = reinterpret_cast<UInt8Type*>(pStack - 7);
+	stack_ = static_cast<bzd::PtrType>(pStack - 7);
 }
