d2c8ccbeb3e3efbf5fce10c59bda7efeeb5b9497
Blaise <blaise.lengrand@gmail.com>
2020-11-14 19:43:18 +0100

    Optimized stack size

--- cc/bzd/platform/stack/x86_64_system_v/stack.cc
+++ cc/bzd/platform/stack/x86_64_system_v/stack.cc
@@ -6,10 +6,9 @@ const bzd::platform::interface::Stack::Direction bzd::platform::interface::Stack
 void bzd::platform::interface::StackUser::reset(const FctPtrType fct) noexcept
 {
 	auto pStack = last<bzd::UInt64Type, 16>();
-	// pStack[0] is for the return address
-	// check why we cannot use -1 or 0
-	//pStack[-2] = nullptr; //<- needs to be removed
-	pStack[-3] = reinterpret_cast<bzd::UInt64Type>(fct);
-	pStack[-10] = (static_cast<bzd::UInt64Type>(0x0) << 32) | 0x1f80; //<- default MXCSR value at reset is 1F80H.
-	stack_ = reinterpret_cast<UInt8Type*>(pStack - 10);
+	// The function address
+	pStack[0] = reinterpret_cast<bzd::UInt64Type>(fct);
+	// MMX & x87, default MXCSR value at reset is 1F80H
+	pStack[-7] = (static_cast<bzd::UInt64Type>(0x0) << 32) | 0x1f80;
+	stack_ = reinterpret_cast<UInt8Type*>(pStack - 7);
 }
