cf7a2d4bd47ac5824256c03239b86847fa1281dc
Blaise <blaise.lengrand@gmail.com>
2020-12-02 22:31:01 +0100

    Adding preset (wip)

--- apps/dashboard/plugins/snmp/plugin.mjs
+++ apps/dashboard/plugins/snmp/plugin.mjs
@@ -4,20 +4,37 @@ export default {
 	visualization: ["system_monitor"],
 	form: [
 		{
-			type: "Dropdown",
-			name: "snmp.preset",
-			caption: "Preset",
-			list: ["Synology"],
-		},
-		{
-			type: "Dropdown",
-			name: "snmp.oids",
+			type: "Array",
+			name: "snmp.array",
 			caption: "OIDs",
-			list: {
-				"1.3.6.1.4.1.6574.1.5.1.0": "model",
-				"1.3.6.1.2.1.25.2.3.1.5.31": "hdd",
-			},
-			multi: true,
+			template: [
+				{ type: "Input", caption: "Identifier", name: "id", width: 0.4 },
+				{ type: "Input", caption: "OID", name: "oid", width: 0.4 },
+				{ type: "Input", caption: "Time To Live", name: "ttl", width: 0.2 },
+			],
+			templateAdd: [
+				{
+					type: "Dropdown",
+					name: "preset",
+					caption: "Preset",
+					list: {
+						synology: "Synology",
+					},
+					width: 0.4,
+				},
+				{
+					type: "Dropdown",
+					name: "oid",
+					caption: "OIDs",
+					condition: (values) => values["preset"] == "synology",
+					list: {
+						"1.3.6.1.4.1.6574.1.5.1.0": "model",
+						"1.3.6.1.2.1.25.2.3.1.5.31": "hdd",
+					},
+					width: 0.4,
+				},
+				{ type: "Button", content: "Add", action: "approve", width: 0.2, align: "bottom" },
+			],
 		},
 	],
 	timeout: 2000, // ms
