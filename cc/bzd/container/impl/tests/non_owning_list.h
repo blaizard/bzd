5cc027a75135b1e4b6ce11f4361e7396bc3e7fb2
blaise <blaise.lengrand@gmail.com>
2021-01-15 18:21:27 +0100

    Cleanup non owning list implementation

---
+++ cc/bzd/container/impl/tests/non_owning_list.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "bzd/container/impl/non_owning_list.h"
+#include "bzd/platform/out.h"
+#include "bzd/utility/format/format.h"
+
+namespace bzd::test {
+
+class ListElementMultiContainer : public bzd::impl::ListElement</*MultiContainer*/ true>
+{
+public:
+	ListElementMultiContainer(bzd::SizeType value) : value_{value} {}
+	ListElementMultiContainer(ListElementMultiContainer&&) = default;
+
+	bzd::SizeType value_;
+};
+
+class ListElement : public bzd::impl::ListElement<false>
+{
+public:
+	ListElement(bzd::SizeType value) : value_{value} {}
+	ListElement(ListElement&&) = default;
+
+	bzd::SizeType value_;
+};
+
+template <class T>
+class NonOwningList : public bzd::impl::NonOwningList<T>
+{
+public:
+	using Self = typename bzd::impl::NonOwningList<T>;
+	using typename Self::BaseElementPtrType;
+
+public:
+	/**
+	 * Ensures that all element are properly connected.
+	 * \return The number of node evaluated.
+	 */
+	template <class U>
+	[[nodiscard]] Result<bzd::SizeType> sanityCheck(const U sanityCheckElement = [](const auto&) -> bool { return true; })
+	{
+		bzd::SizeType counter = 0;
+		auto previous = &this->front_;
+		auto node = Self::removeMarks(previous->next_.load());
+
+		printNode(previous);
+
+		while (true)
+		{
+			bzd::assert::isTrue(!Self::isDeletionMark(node), "Deletion mark");
+			bzd::assert::isTrue(!Self::isInsertionMark(node), "Insertion mark");
+			bzd::assert::isTrue(node, "Null node");
+
+			printNode(node);
+
+			// Check if the end is reached.
+			if (node == &this->back_)
+			{
+				const auto maybeBack = Self::back();
+				if (!maybeBack)
+				{
+					bzd::assert::isTrue(counter == 0, CSTR("Expected 0 size but got: {}"), counter);
+				}
+				else
+				{
+					bzd::assert::isTrue(previous == &(*maybeBack),
+										CSTR("Last pointer is not pointing to the last element: {} vs {}"),
+										previous,
+										&(*maybeBack));
+				}
+
+				break;
+			}
+
+			// Check the element.
+			{
+				const bool result = sanityCheckElement(reinterpret_cast<T&>(*node));
+				bzd::assert::isTrue(result, "Custom sanityCheckElement failed");
+			}
+
+			++counter;
+			previous = node;
+			node = node->next_.load();
+		}
+
+		// Element count should be equal.
+		bzd::assert::isTrue(Self::size() == counter, CSTR("Counter ({}) mistmatch with actual size ({})"), counter, Self::size());
+
+		return counter;
+	}
+
+	constexpr void printChain(bool onlyIfFails = true, bzd::SizeType maxElements = 100)
+	{
+		BaseElementPtrType cur = &this->front_;
+		while (--maxElements && cur && cur != &this->back_)
+		{
+			if (!onlyIfFails)
+			{
+				printNode(cur);
+			}
+			cur = removeDeletionMark(cur->next_.load());
+		}
+		if (maxElements == 0)
+		{
+			if (onlyIfFails)
+			{
+				printChain(/*onlyIfFails*/ false);
+			}
+			bzd::assert::unreachable();
+		}
+	}
+
+	void printNode(BaseElementPtrType node)
+	{
+		bzd::StringChannel<256> stream;
+		const auto printAddress = [this, &stream](const BaseElementPtrType address) {
+			bzd::format::toString(bzd::platform::getOut(),
+								  CSTR("{} {}"),
+								  address,
+								  (address == &this->front_) ? " (F)" : ((address == &this->back_) ? " (B)" : "    "));
+		};
+
+		printAddress(node);
+		if (node)
+		{
+			stream.write(" -> ");
+			printAddress(node->next_.load());
+		}
+		stream.write("\n");
+		bzd::platform::getOut().write(stream.str());
+	}
+};
+} // namespace bzd::test
