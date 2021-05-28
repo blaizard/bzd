b199fd8ee5f6640386ae1f95188f35e39116e19f
blaise <blaise.lengrand@gmail.com>
2021-05-28 22:43:50 +0200

    Adding bdl syntax

---
+++ tools/vscode/README.md
@@ -0,0 +1,7 @@
+# VSCode extension for bdl language
+
+This adds syntax highlighting for .bdl extenstions.
+
+## Debug
+
+`Ctrl + Shift + P` and then type `Developer: Inspect Editor Tokens and Scopes`, this will highlight tokens on a.bdl file.
--- tools/vscode/install.sh
+++ tools/vscode/install.sh
@@ -7,4 +7,3 @@ rm -rfd "${VSCODE_EXTENSION}/bdl"
 cp -r "${SCRIPTPATH}/bdl" "${VSCODE_EXTENSION}"
 
 # https://stackoverflow.com/questions/42116486/where-can-i-find-a-list-of-all-possible-keys-for-tm-themes-for-syntax-highlighti
-# https://stackoverflow.com/questions/54356746/how-do-i-add-a-bare-bones-syntax-to-vscode
