b2c64145e4e046f39f5b3f35436f5071677457e3
Blaise <blaise.lengrand@gmail.com>
2021-05-30 00:17:34 +0200

    Adding builtin deps to bzd_manifest

---
+++ cc/bzd/platform/core/linux/main.cc
@@ -0,0 +1,16 @@
+#include "cc/bzd/platform/core/linux/core.h"
+
+#include <iostream>
+
+int main()
+{
+	static bzd::platform::core::Linux<100000> linux{};
+	auto workload = []() {
+		std::cout << "Workload" << std::endl;
+	};
+	linux.run(static_cast<WorkloadType>(workload));
+
+	std::cout << linux.getStackUsage() << std::endl;
+
+	return 0;
+}
