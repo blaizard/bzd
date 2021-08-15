86cfcd436afb98cd3bdd66a96fc4ba674e95d211
blaizard <blaise.lengrand@gmail.com>
2021-08-15 20:53:30 +0200

    Adding accessors for entity sequence

--- tools/bdl/entities/impl/fragment/sequence.py
+++ tools/bdl/entities/impl/fragment/sequence.py
@@ -1,7 +1,9 @@
 import typing
 
+from collections import defaultdict
+
 if typing.TYPE_CHECKING:
-	from tools.bdl.entities.all import EntityType
+	from tools.bdl.entities.all import Expression, Builtin, Nested, Method, Using, Enum, Namespace, Use, EntityType
 
 
 class EntitySequence:
@@ -11,6 +13,7 @@ class EntitySequence:
 
 	def __init__(self, sequence: typing.List["EntityType"]) -> None:
 		self.list = sequence
+		self.processed: typing.Optional[typing.Dict[str, typing.List["EntityType"]]] = None
 
 	def __iter__(self) -> typing.Iterator["EntityType"]:
 		for entity in self.list:
@@ -24,3 +27,69 @@ class EntitySequence:
 
 	def __repr__(self) -> str:
 		return "\n".join([str(entity) for entity in self.list])
+
+	def _filteredList(self, category: str) -> typing.List["EntityType"]:
+		if not self.processed:
+			self.processed = defaultdict(list)
+			for entity in self.list:
+				self.processed[entity.category].append(entity)
+		return self.processed.get(category, [])
+
+	def _is(self, category: str) -> bool:
+		return bool(self._filteredList(category))
+
+	@property
+	def isNested(self) -> bool:
+		return self._is("nested")
+
+	@property
+	def nestedList(self) -> typing.List["Nested"]:
+		return typing.cast(typing.List["Nested"], self._filteredList("nested"))
+
+	@property
+	def isExpression(self) -> bool:
+		return self._is("expression")
+
+	@property
+	def expressionList(self) -> typing.List["Expression"]:
+		return typing.cast(typing.List["Expression"], self._filteredList("expression"))
+
+	@property
+	def isMethod(self) -> bool:
+		return self._is("method")
+
+	@property
+	def methodList(self) -> typing.List["Method"]:
+		return typing.cast(typing.List["Method"], self._filteredList("method"))
+
+	@property
+	def isUsing(self) -> bool:
+		return self._is("using")
+
+	@property
+	def usingList(self) -> typing.List["Using"]:
+		return typing.cast(typing.List["Using"], self._filteredList("using"))
+
+	@property
+	def isEnum(self) -> bool:
+		return self._is("enum")
+
+	@property
+	def enumList(self) -> typing.List["Enum"]:
+		return typing.cast(typing.List["Enum"], self._filteredList("enum"))
+
+	@property
+	def isNamespace(self) -> bool:
+		return self._is("namespace")
+
+	@property
+	def namespaceList(self) -> typing.List["Namespace"]:
+		return typing.cast(typing.List["Namespace"], self._filteredList("namespace"))
+
+	@property
+	def isUse(self) -> bool:
+		return self._is("use")
+
+	@property
+	def useList(self) -> typing.List["Use"]:
+		return typing.cast(typing.List["Use"], self._filteredList("use"))
