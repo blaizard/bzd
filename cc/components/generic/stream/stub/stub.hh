f35244b1c08cb41e99e37e5d1af6cd5598a7e024
blaizard <blaise.lengrand@gmail.com>
2021-10-26 22:37:04 +0200

    Result makes use of variant

--- cc/components/generic/stream/stub/stub.hh
+++ cc/components/generic/stream/stub/stub.hh
@@ -8,6 +8,6 @@ class Stub : public bzd::IOStream
 public:
 	bzd::Async<SizeType> write(const bzd::Span<const bzd::ByteType> data) noexcept override { co_return data.size(); }
 
-	bzd::Async<SizeType> read(const bzd::Span<bzd::ByteType>) noexcept override { co_return 0; }
+	bzd::Async<SizeType> read(const bzd::Span<bzd::ByteType>) noexcept override { co_return 0U; }
 };
 } // namespace bzd::platform::generic::stream
