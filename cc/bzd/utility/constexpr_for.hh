f574320e3b494a9e0a721e73ec446a02adc7a613
blaise <blaise.lengrand@gmail.com>
2021-10-30 18:25:07 +0200

    Cleanup

--- cc/bzd/utility/constexpr_for.hh
+++ cc/bzd/utility/constexpr_for.hh
@@ -1,18 +1,19 @@
 #pragma once
 
+#include "cc/bzd/platform/types.hh"
 #include "cc/bzd/type_traits/decay.hh"
 #include "cc/bzd/type_traits/integral_constant.hh"
 
 namespace bzd {
 
-template <int Start, int End, int Inc, class F>
+template <Int32Type Start, Int32Type End, Int32Type Inc, class F>
 constexpr void constexprFor(F&& f) noexcept
 {
 	static_assert(Inc != 0, "The increment cannot be equal to 0.");
 
 	if constexpr ((Inc > 0 && Start < End) || (Inc < 0 && Start > End))
 	{
-		f(bzd::typeTraits::IntegralConstant<decltype(Start), Start>());
+		f(typeTraits::IntegralConstant<decltype(Start), Start>());
 		constexprFor<Start + Inc, End, Inc>(f);
 	}
 }
@@ -26,7 +27,7 @@ constexpr void constexprForContainerInc(Container&& container, F&& f) noexcept
 template <class Container, class F>
 constexpr void constexprForContainerDec(Container&& container, F&& f) noexcept
 {
-	constexprFor<bzd::typeTraits::Decay<Container>::size() - 1, -1, -1>([&](auto i) { f(container.template get<i.value>()); });
+	constexprFor<static_cast<Int32Type>(typeTraits::Decay<Container>::size()) - 1, -1, -1>([&](auto i) { f(container.template get<i.value>()); });
 }
 
 } // namespace bzd
