dcb82148d4f344da475bd936a9e52c9f65cc4a8c
blaise <blaise.lengrand@gmail.com>
2021-12-02 23:32:31 +0100

    Using initializer list

--- cc/bzd/container/storage/fixed.hh
+++ cc/bzd/container/storage/fixed.hh
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "cc/bzd/algorithm/copy_n.hh"
+#include "cc/bzd/container/iterator/distance.hh"
 #include "cc/bzd/platform/types.hh"
 #include "cc/bzd/utility/forward.hh"
+#include "cc/bzd/utility/in_place.hh"
+#include "cc/bzd/utility/min.hh"
 
 namespace bzd::impl {
 /// \brief Fixed storage type.
@@ -22,8 +26,14 @@ public: // Constructors
 	constexpr Self& operator=(Self&&) noexcept = default;
 
 	template <class... Args>
-	constexpr explicit FixedStorage(Args&&... args) noexcept : data_{bzd::forward<Args>(args)...}
+	constexpr explicit FixedStorage(InPlace, Args&&... args) noexcept : data_{bzd::forward<Args>(args)...}
 	{
+	}
+
+	constexpr FixedStorage(const T* begin, const T* end) noexcept
+	{
+		const auto n = bzd::min(N, static_cast<SizeType>(end - begin));
+		bzd::algorithm::copyN(begin, n, data_);
 	}
 
 public: // Accessors
