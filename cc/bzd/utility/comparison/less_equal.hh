4f668fce0204db6446539ea9f0025ee4770c6ee5
blaise <blaise.lengrand@gmail.com>
2021-12-08 21:42:04 +0100

    sanitize

--- cc/bzd/utility/comparison/less_equal.hh
+++ cc/bzd/utility/comparison/less_equal.hh
@@ -2,14 +2,10 @@
 
 #include "cc/bzd/platform/types.hh"
 
-namespace bzd
-{
+namespace bzd {
 template <class T>
 struct LessEqual
 {
-    constexpr BoolType operator()(const T& lhs, const T& rhs) const 
-    {
-        return lhs <= rhs;
-    }
+	constexpr BoolType operator()(const T& lhs, const T& rhs) const { return lhs <= rhs; }
 };
-}
+} // namespace bzd
--- cc/bzd/utility/numeric_limits.hh
+++ cc/bzd/utility/numeric_limits.hh
@@ -2,64 +2,63 @@
 
 #include "cc/bzd/platform/types.hh"
 
-namespace bzd
-{
+namespace bzd {
 template <class T>
 struct NumericLimits;
 
 template <>
 struct NumericLimits<UInt8Type>
 {
-    [[nodiscard]] static constexpr UInt8Type min() noexcept { return 0; }
-    [[nodiscard]] static constexpr UInt8Type max() noexcept { return 0xff; }
+	[[nodiscard]] static constexpr UInt8Type min() noexcept { return 0; }
+	[[nodiscard]] static constexpr UInt8Type max() noexcept { return 0xff; }
 };
 
 template <>
 struct NumericLimits<UInt16Type>
 {
-    [[nodiscard]] static constexpr UInt16Type min() noexcept { return 0; }
-    [[nodiscard]] static constexpr UInt16Type max() noexcept { return 0xffff; }
+	[[nodiscard]] static constexpr UInt16Type min() noexcept { return 0; }
+	[[nodiscard]] static constexpr UInt16Type max() noexcept { return 0xffff; }
 };
 
 template <>
 struct NumericLimits<UInt32Type>
 {
-    [[nodiscard]] static constexpr UInt32Type min() noexcept { return 0; }
-    [[nodiscard]] static constexpr UInt32Type max() noexcept { return 0xffffffff; }
+	[[nodiscard]] static constexpr UInt32Type min() noexcept { return 0; }
+	[[nodiscard]] static constexpr UInt32Type max() noexcept { return 0xffffffff; }
 };
 
 template <>
 struct NumericLimits<UInt64Type>
 {
-    [[nodiscard]] static constexpr UInt64Type min() noexcept { return 0; }
-    [[nodiscard]] static constexpr UInt64Type max() noexcept { return 0xffffffffffffffff; }
+	[[nodiscard]] static constexpr UInt64Type min() noexcept { return 0; }
+	[[nodiscard]] static constexpr UInt64Type max() noexcept { return 0xffffffffffffffff; }
 };
 
 template <>
 struct NumericLimits<Int8Type>
 {
-    [[nodiscard]] static constexpr Int8Type min() noexcept { return -0x80; }
-    [[nodiscard]] static constexpr Int8Type max() noexcept { return 0x7f; }
+	[[nodiscard]] static constexpr Int8Type min() noexcept { return -0x80; }
+	[[nodiscard]] static constexpr Int8Type max() noexcept { return 0x7f; }
 };
 
 template <>
 struct NumericLimits<Int16Type>
 {
-    [[nodiscard]] static constexpr Int16Type min() noexcept { return -0x8000; }
-    [[nodiscard]] static constexpr Int16Type max() noexcept { return 0x7fff; }
+	[[nodiscard]] static constexpr Int16Type min() noexcept { return -0x8000; }
+	[[nodiscard]] static constexpr Int16Type max() noexcept { return 0x7fff; }
 };
 
 template <>
 struct NumericLimits<Int32Type>
 {
-    [[nodiscard]] static constexpr Int32Type min() noexcept { return -0x80000000; }
-    [[nodiscard]] static constexpr Int32Type max() noexcept { return 0x7fffffff; }
+	[[nodiscard]] static constexpr Int32Type min() noexcept { return -0x80000000; }
+	[[nodiscard]] static constexpr Int32Type max() noexcept { return 0x7fffffff; }
 };
 
 template <>
 struct NumericLimits<Int64Type>
 {
-    [[nodiscard]] static constexpr Int64Type min() noexcept { return -0x8000000000000000; }
-    [[nodiscard]] static constexpr Int64Type max() noexcept { return 0x7fffffffffffffff; }
+	[[nodiscard]] static constexpr Int64Type min() noexcept { return -0x8000000000000000; }
+	[[nodiscard]] static constexpr Int64Type max() noexcept { return 0x7fffffffffffffff; }
 };
-}
+} // namespace bzd
