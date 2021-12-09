56ba6b1b59a5ba9c0e1bdf7bacd797bbf112d389
blaizard <blaise.lengrand@gmail.com>
2021-12-09 23:43:49 +0100

    Adding upperBound and binarySearch

---
+++ cc/bzd/algorithm/tests/lower_bound.cc
@@ -0,0 +1,58 @@
+#include "cc/bzd/algorithm/lower_bound.hh"
+
+#include "cc/bzd/container/array.hh"
+#include "cc/bzd/test/test.hh"
+
+TEST(LowerBound, Base)
+{
+	bzd::Array<int, 5> array{0, 1, 2, 3, 5};
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 1);
+        EXPECT_EQ(*it, 1);
+    }
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 0);
+        EXPECT_EQ(*it, 0);
+    }
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 5);
+        EXPECT_EQ(*it, 5);
+    }
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), -2);
+        EXPECT_EQ(*it, 0);
+    }
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 10);
+        EXPECT_EQ(it, array.end());
+    }
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 4);
+        EXPECT_EQ(*it, 5);
+    }
+}
+
+TEST(LowerBound, EdgeCase)
+{
+	bzd::Array<int, 5> array{1, 2, 3, 4, 5};
+
+    {
+        const auto it = bzd::algorithm::lowerBound(array.begin(), array.begin(), 1);
+        EXPECT_EQ(it, array.begin());
+    }
+}
+
+TEST_CONSTEXPR_BEGIN(LowerBound, Constexpr)
+{
+	bzd::Array<int, 5> array{0, 1, 2, 3, 5};
+
+    const auto it = bzd::algorithm::lowerBound(array.begin(), array.end(), 0);
+    EXPECT_EQ(*it, 0);
+}
+TEST_CONSTEXPR_END(LowerBound, Constexpr)
--- cc/bzd/algorithm/upper_bound.hh
+++ cc/bzd/algorithm/upper_bound.hh
@@ -8,9 +8,9 @@
 namespace bzd::algorithm {
 
 template <class Iterator, class T, class Compare = bzd::Less<typename Iterator::ValueType>>
-constexpr Iterator lowerBound(Iterator first, Iterator last, const T& value, Compare comparison = Compare{})
+constexpr Iterator upperBound(Iterator first, Iterator last, const T& value, Compare comparison = Compare{})
 {
-	static_assert(typeTraits::isIterator<Iterator>, "Only iterators can be used with lowerBound.");
+	static_assert(typeTraits::isIterator<Iterator>, "Only iterators can be used with upperBound.");
 	static_assert(iterator::isCategory<Iterator, iterator::ForwardTag>, "The iterator must be a forward iterator.");
 
 	using DifferenceType = typename bzd::iterator::Traits<Iterator>::DifferenceType;
@@ -24,7 +24,7 @@ constexpr Iterator lowerBound(Iterator first, Iterator last, const T& value, Com
 		it = first;
 		step = count / 2;
 		bzd::iterator::advance(it, step);
-		if (comparison(*it, value))
+		if (!comparison(value, *it))
 		{
 			first = ++it;
 			count -= step + 1;
