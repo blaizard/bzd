2f7524679f8cd306d7889c564f1c447622056dfb
blaise <blaise.lengrand@gmail.com>
2021-12-17 17:37:12 +0100

    Fixed CI

--- tools/ci/jenkins/run_tests.sh
+++ tools/ci/jenkins/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "/home/1000/sandbox"
-./tools/bazel test ... --output_groups=default,metadata --config=linux_x86_64_clang --config=dev --platform_suffix=_dev
+./tools/bazel test ... --output_groups=+metadata --config=linux_x86_64_clang --config=dev --platform_suffix=_dev
