ec2bd5ca6510559ff3ba754e745f51eb35024056
blaise <blaise.lengrand@gmail.com>
2022-03-02 22:20:34 +0100

    Completed non owning list implementation

--- cc/bzd/container/non_owning_list.hh
+++ cc/bzd/container/non_owning_list.hh
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "cc/bzd/container/result.hh"
+#include "cc/bzd/type_traits/add_const.hh"
+#include "cc/bzd/type_traits/conditional.hh"
+#include "cc/bzd/type_traits/is_const.hh"
+#include "cc/bzd/type_traits/iterator.hh"
+#include "cc/bzd/utility/ignore.hh"
 
 namespace bzd {
 enum class NonOwningListErrorType
@@ -15,21 +20,95 @@ struct NonOwningListElement
 	NonOwningListElement* next_{nullptr};
 	NonOwningListElement* previous_{nullptr};
 };
+} // namespace bzd
+
+namespace bzd::impl {
+template <class U>
+class NonOwningListIterator
+{
+public: // Traits
+	using Self = NonOwningListIterator<U>;
+	using Category = bzd::typeTraits::ForwardTag;
+	using IndexType = bzd::SizeType;
+	using DifferenceType = bzd::Int32Type;
+	using ValueType = U;
+
+private: // Internal Traits.
+	using UnderlyingValuePtrType =
+		typeTraits::Conditional<bzd::typeTraits::isConst<ValueType>, const NonOwningListElement, NonOwningListElement>*;
+
+public:
+	constexpr NonOwningListIterator(UnderlyingValuePtrType ptr) noexcept : current_{ptr} {}
+
+public: // Modifiers.
+	constexpr Self& operator++() noexcept
+	{
+		next();
+		return *this;
+	}
+
+	constexpr Self operator++(int) noexcept
+	{
+		Self it{*this};
+		++(*this);
+		return it;
+	}
+
+public: // Comparators.
+	[[nodiscard]] constexpr bool operator==(const Self& other) const noexcept { return (current_ == other.current_); }
+	[[nodiscard]] constexpr bool operator!=(const Self& other) const noexcept { return !(other == *this); }
+
+public: // Accessors.
+	[[nodiscard]] constexpr ValueType& operator*() const { return *static_cast<ValueType*>(current_); }
+	[[nodiscard]] constexpr ValueType* operator->() const { return static_cast<ValueType*>(current_); }
+
+private:
+	constexpr void next() noexcept
+	{
+		bzd::assert::isTrue(current_->next_);
+		current_ = current_->next_;
+	}
+
+private:
+	UnderlyingValuePtrType current_;
+};
+} // namespace bzd::impl
+
+namespace bzd {
 
 template <class T>
 class NonOwningList
 {
-public:
+public: // Traits.
 	template <class V>
 	using Result = bzd::Result<V, NonOwningListErrorType>;
+	using Iterator = bzd::impl::NonOwningListIterator<T>;
+	using ConstIterator = bzd::impl::NonOwningListIterator<bzd::typeTraits::AddConst<T>>;
 
 public:
 	constexpr NonOwningList() noexcept : first_{&last_, nullptr}, last_{nullptr, &first_} {}
+
+public: // Iterator.
+	/// Return a begin iterator for this list.
+	[[nodiscard]] constexpr auto begin() noexcept { return Iterator{first_.next_}; }
+	[[nodiscard]] constexpr auto begin() const noexcept { return ConstIterator{first_.next_}; }
+
+	/// Return an end iterator for this list.
+	[[nodiscard]] constexpr auto end() noexcept { return Iterator{&last_}; }
+	[[nodiscard]] constexpr auto end() const noexcept { return ConstIterator{&last_}; }
 
 public:
 	[[nodiscard]] constexpr bzd::SizeType size() const noexcept { return size_; }
 
 	[[nodiscard]] constexpr bzd::BoolType empty() const noexcept { return (size_ == 0U); }
+
+	constexpr void clear() noexcept
+	{
+		while (!empty())
+		{
+			bzd::ignore = popFront();
+		}
+	}
 
 	[[nodiscard]] constexpr Result<void> pushFront(T& element) noexcept { return insert(first_, element); }
 
@@ -53,7 +132,7 @@ public:
 		return nullresult;
 	}
 
-	[[nodiscard]] constexpr Result<void> popFront() noexcept
+	[[nodiscard]] constexpr Result<T&> popFront() noexcept
 	{
 		if (empty())
 		{
@@ -62,7 +141,7 @@ public:
 		return erase(*(first_.next_));
 	}
 
-	[[nodiscard]] constexpr Result<void> popBack() noexcept
+	[[nodiscard]] constexpr Result<T&> popBack() noexcept
 	{
 		if (empty())
 		{
@@ -71,7 +150,7 @@ public:
 		return erase(*(last_.previous_));
 	}
 
-	[[nodiscard]] constexpr Result<void> erase(NonOwningListElement& element) noexcept
+	[[nodiscard]] constexpr Result<T&> erase(NonOwningListElement& element) noexcept
 	{
 		if (element.next_ == nullptr)
 		{
@@ -85,7 +164,7 @@ public:
 
 		--size_;
 
-		return nullresult;
+		return static_cast<T&>(element);
 	}
 
 private:
