19ab759f384ee367248f20a0af246f0590edd813
blaizard <blaise.lengrand@gmail.com>
2022-03-21 23:15:42 +0100

    Disabled cache

--- tools/shell/sh/srcs/interactive/update.sh
+++ tools/shell/sh/srcs/interactive/update.sh
@@ -2,6 +2,6 @@
 bzd_update()
 {
     url="https://raw.githubusercontent.com/blaizard/cpp-async/master/tools/shell/sh/bashrc.sh"
-	wget -q "$url" -O ~/.bzd/bashrc.sh
+	wget -q --no-cache "$url" -O ~/.bzd/bashrc.sh
 	bzd_reload
 }
