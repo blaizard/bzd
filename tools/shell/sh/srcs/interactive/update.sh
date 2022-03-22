c44c883306779a555bcb256579c93868f70ad945
blaise <blaise.lengrand@gmail.com>
2022-03-22 21:04:54 +0100

    Improved update

--- tools/shell/sh/srcs/interactive/update.sh
+++ tools/shell/sh/srcs/interactive/update.sh
@@ -2,6 +2,7 @@
 bzd_update()
 {
     url="https://raw.githubusercontent.com/blaizard/cpp-async/master/tools/shell/sh/bashrc.sh"
-	wget -q --no-cache "$url" -O ~/.bzd/bashrc.sh
+	wget -q --no-cache "$url" -O ~/.bzd_update_temp
+	rm -rfd ~/.bzd && mkdir -p ~/.bzd && mv ~/.bzd_update_temp ~/.bzd/bashrc.sh
 	bzd_reload
 }
