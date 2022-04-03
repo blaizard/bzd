3f48edb4783ba5245cb1aa2cb53e9ef174e440e8
blaizard <blaise.lengrand@gmail.com>
2022-04-03 21:07:52 +0200

    Adding jardinier 2000

---
+++ apps/jardinier2000/jardinier.cc
@@ -0,0 +1,28 @@
+#include "apps/jardinier2000/jardinier.hh"
+
+#include <driver/gpio.h>
+#include <esp_sleep.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+namespace jardinier {
+
+constexpr gpio_num_t waterPumpPin{GPIO_NUM_23};
+
+bzd::Async<bool> water(bzd::SizeType wateringTimeS, bzd::UInt64Type wakeUpPeriodS)
+{
+	co_await bzd::log::info(CSTR("Watering for {}s..."), wateringTimeS);
+	gpio_pad_select_gpio(waterPumpPin);
+	gpio_set_direction(waterPumpPin, GPIO_MODE_OUTPUT);
+	gpio_set_level(waterPumpPin, 1);
+	vTaskDelay(configTICK_RATE_HZ * wateringTimeS);
+	gpio_set_level(waterPumpPin, 0);
+
+	co_await bzd::log::info(CSTR("Going to sleep, will wake up in {}s..."), wakeUpPeriodS);
+	esp_sleep_enable_timer_wakeup(/*time us*/ wakeUpPeriodS * 1000000);
+	esp_deep_sleep_start();
+
+	co_return true;
+}
+
+} // namespace jardinier
