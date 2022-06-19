f20bc0d92e07bcd9ab259eaaa2cc25dc6044d0a0
blaise <blaise.lengrand@gmail.com>
2022-06-19 15:15:54 +0200

    Sanitize

--- cc/components/posix/network/tcp/server.hh
+++ cc/components/posix/network/tcp/server.hh
@@ -5,24 +5,19 @@ template <class Handler>
 class Server
 {
 public:
-    constexpr Server(const StringView endpoint, Handler& handler)
-    {
-    }
+	constexpr Server(const StringView endpoint, Handler& handler) {}
 
-    auto init()
-    {
-        socket_ = endpoint.createSocket(endpoint_);
-    }
+	auto init() { socket_ = endpoint.createSocket(endpoint_); }
 
 private:
-    void onNewConnection()
-    {
-        connection = accept();
-        Stream stream{connection};
-        handler.onNewConnection(bzd::move(stream));
-    }
+	void onNewConnection()
+	{
+		connection = accept();
+		Stream stream{connection};
+		handler.onNewConnection(bzd::move(stream));
+	}
 };
-}
+} // namespace bzd::platform::posix::network::tcp
 
 /*
 Usage in bdl:
@@ -34,10 +29,10 @@ Usage in C++:
 class Webserver
 {
 public:
-    void onNewConnection(auto connection)
-    {
-        co_await connection.read();
-        co_await connection.write();
-    }
+	void onNewConnection(auto connection)
+	{
+		co_await connection.read();
+		co_await connection.write();
+	}
 };
 */
