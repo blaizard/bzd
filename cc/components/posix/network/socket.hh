3dbf9079a272c9a5b7c1a99408a75013afe272af
blaise <blaise.lengrand@gmail.com>
2022-07-04 22:36:29 +0200

    Adding addresses for posix

--- cc/components/posix/network/socket.hh
+++ cc/components/posix/network/socket.hh
@@ -1,54 +1,12 @@
 #pragma once
 
-#include "cc/bzd/utility/to_underlying.hh"
 #include "cc/components/posix/io/file_descriptor.hh"
+#include "cc/components/posix/network/address/address.hh"
 #include "cc/components/posix/network/address/family.hh"
 
 #include <sys/socket.h>
 
 namespace bzd::platform::posix::network {
-
-/// Type for a socket.
-enum class SocketType
-{
-	/// Provides sequenced, reliable, two-way, connection-based
-	/// byte streams.  An out-of-band data transmission mechanism
-	/// may be supported.
-	stream = SOCK_STREAM,
-	/// Supports datagrams (connectionless, unreliable messages of
-	/// a fixed maximum length).
-	datagram = SOCK_DGRAM,
-	/// Provides a sequenced, reliable, two-way connection-based
-	/// data transmission path for datagrams of fixed maximum
-	/// length; a consumer is required to read an entire packet
-	/// with each input system call.
-	sequencedPacket = SOCK_SEQPACKET,
-	/// Provides raw network protocol access.
-	raw = SOCK_RAW,
-	/// Provides a reliable datagram layer that does not guarantee
-	/// ordering.
-	reliable = SOCK_RDM,
-};
-
-/// The followings are bitwise OR options.
-enum class SocketTypeOption
-{
-	/// Set the O_NONBLOCK file status flag on the open file
-	/// description (see open(2)) referred to by the new file
-	/// descriptor.  Using this flag saves extra calls to fcntl(2)
-	/// to achieve the same result.
-	nonBlocking = SOCK_NONBLOCK,
-	/// Set the close-on-exec (FD_CLOEXEC) flag on the new file
-	/// descriptor.  See the description of the O_CLOEXEC flag in
-	/// open(2) for reasons why this may be useful.
-	closeOnExec = SOCK_CLOEXEC
-};
-
-/// Bit or operator to combine a socket type with optional option(s).
-constexpr SocketType operator|(const SocketType a, const SocketTypeOption b) noexcept
-{
-	return static_cast<SocketType>(bzd::toUnderlying(a) | bzd::toUnderlying(b));
-}
 
 class Socket
 {
@@ -57,7 +15,10 @@ private:
 
 public:
 	/// Create a socket.
-	static bzd::Result<Socket, bzd::Error> make(const AddressFamily family, const SocketType type, const int protocol) noexcept;
+	static bzd::Result<Socket, bzd::Error> make(const AddressFamily family, const SocketType type, const int protocol = 0) noexcept;
+
+	/// Create a socket from an address.
+	static bzd::Result<Socket, bzd::Error> make(const Address& address, const SocketTypeOption options) noexcept;
 
 public:
 	/// Get its associated file descriptor object.
--- cc/components/posix/network/tests/socket.cc
+++ cc/components/posix/network/tests/socket.cc
@@ -2,10 +2,11 @@
 
 #include "cc/bzd/test/test.hh"
 
+namespace bzd::platform::posix::network {
+
 TEST(Socket, constructor)
 {
-	const auto maybeSocket = bzd::platform::posix::network::Socket::make(
-		bzd::platform::posix::network::AddressFamily::ipV4,
-		bzd::platform::posix::network::SocketType::datagram | bzd::platform::posix::network::SocketTypeOption::closeOnExec,
-		0);
+	const auto maybeSocket = Socket::make(AddressFamily::ipV4, SocketType::datagram | SocketTypeOption::closeOnExec, 0);
 }
+
+} // namespace bzd::platform::posix::network
