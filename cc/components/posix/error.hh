87637e2e19d6b3eb9963f8e2b93e8ab304e4dd58
blaise <blaise.lengrand@gmail.com>
2022-07-12 19:51:12 +0200

    sanitize

--- cc/components/posix/error.hh
+++ cc/components/posix/error.hh
@@ -10,6 +10,7 @@ namespace bzd::error {
 /// Generate an error from a POSIX API and feed it with debug information.
 struct Posix : public bzd::ResultError<bzd::Error>
 {
+	// NOLINTNEXTLINE(bugprone-exception-escape)
 	Posix(const bzd::StringView function, const SourceLocation location = SourceLocation::current()) noexcept :
 		bzd::ResultError<bzd::Error>{
 			location, ErrorType::failure, "POSIX function '{}', errno {}, '{}'"_csv, function, errno, ::strerror(errno)}
