74e20f00a3e863867448005c8c407c8b7eb086c4
blaizard <blaise.lengrand@gmail.com>
2022-07-19 22:15:39 +0200

    Adding support for inheritance

--- tools/bdl/generators/cc/adapter/types.hh
+++ tools/bdl/generators/cc/adapter/types.hh
@@ -21,16 +21,19 @@ namespace impl {
 /// \param obj The current object containing the interface (usually 'this').
 /// \param interface The member pointer of the interface function.
 /// \param impl The member pointer of the implementation function.
-/// \return A point of the implementation object.
-template <class T, class Interface, class Impl>
-constexpr auto* getMethod(T* obj, Interface interface, Impl impl) noexcept
+/// \return A pointer of the implementation object.
+template <template <class> class T, class Impl, class InterfaceMember, class ImplMember>
+constexpr auto* getMethod(T<Impl>* obj, InterfaceMember interface, ImplMember impl) noexcept
 {
 	using TraitsInterface = bzd::typeTraits::Function<decltype(interface)>;
 	using TraitsImpl = bzd::typeTraits::Function<decltype(impl)>;
 	static_assert(bzd::typeTraits::isSame<typename TraitsInterface::Signature, typename TraitsImpl::Signature>,
 				  "Signature doesn't match its interface.");
 	static_assert(TraitsInterface::isNoexcept && TraitsImpl::isNoexcept, "Interface must have the same exception guaranty.");
-	return static_cast<typename TraitsImpl::Class*>(obj);
+	// Note: the implement class needs to be deducted from the template argument of the object.
+	// This cannot be done from TraitsImpl::Class, as with inheritance, this can resolve to
+	// implementation class that is not a child of this object.
+	return static_cast<Impl*>(obj);
 }
 
 /// Helper to get the interface from the implementation.
