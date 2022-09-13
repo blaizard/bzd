06aa4792fa30fdc630345b1d4b535ee031c80809
blaizard <blaise.lengrand@gmail.com>
2022-09-13 21:13:23 +0200

    Adding channel_reader

--- cc/libs/reader/channel_reader.hh
+++ cc/libs/reader/channel_reader.hh
@@ -17,8 +17,6 @@ public:
 	constexpr explicit ChannelReader(bzd::IChannel<T>& in) noexcept : in_{in} {}
 
 	/// Unconditional read from the input channel.
-	///
-	/// Note this function might return a buffer that is different from the one passed into argument.
 	bzd::Async<bzd::Span<const T>> read(bzd::Span<T>&& data) noexcept final
 	{
 		const auto readFromBuffer = buffer_.asSpanForReading();
@@ -30,100 +28,153 @@ public:
 		buffer_.consume(count);
 		co_return readFromBuffer.first(count);
 	}
-/*
-	/// Problem with this solution:
-	/// - Cannot use an input buffer to write data directly to it. Need to copy first to internal buffer.
-	///       -> Wrong concern because data cannot be written to the output buffer directly as it needs to be read first.
-	///              -> /!\ This is not always true for other type of functions, for example readNumberOfBytes(...).
-	///       -> produce() can be changed to not produce data to the ring (so use .read instead of .readToBuffer).
-	/// - Not compatible with IChannel<T>
-	///       -> 
-	///
-	/// Advantages:
-	/// - Simple to use.
-	/// - The function is stateful, making it easier to write the logic.
-	bzd::Generator<bzd::Span<const T>> readUntil(const T& value) noexcept
-	{
-		auto readFromBuffer = buffer_.asSpanForReading();
-		const Size position;
-		while (position = bzd::algorithm::find(readFromBuffer, value); position == readFromBuffer.end())
+	/*
+		/// Problem with this solution:
+		/// - Cannot use an input buffer to write data directly to it. Need to copy first to internal buffer.
+		///       -> Wrong concern because data cannot be written to the output buffer directly as it needs to be read first.
+		///              -> /!\ This is not always true for other type of functions, for example readNumberOfBytes(...).
+		///       -> produce() can be changed to not produce data to the ring (so use .read instead of .readToBuffer).
+		/// - Not compatible with IChannel<T>
+		///       ->
+		///
+		/// Advantages:
+		/// - Simple to use.
+		/// - The function is stateful, making it easier to write the logic.
+		bzd::Generator<bzd::Span<const T>> readUntil(const T& value) noexcept
 		{
-			buffer_.consume(readFromBuffer.size());
-			co_yield readFromBuffer;
-			readFromBuffer = co_await !produce();
+			auto readFromBuffer = buffer_.asSpanForReading();
+			const Size position;
+			while (position = bzd::algorithm::find(readFromBuffer, value); position == readFromBuffer.end())
+			{
+				buffer_.consume(readFromBuffer.size());
+				co_yield readFromBuffer;
+				readFromBuffer = co_await !produce();
+			}
+			buffer_.consume(position);
+			co_yield readFromBuffer.first(position);
 		}
-		buffer_.consume(position);
-		co_yield readFromBuffer.first(position);
-	}
 
 
-template <class T>
-class IFiniteChannel : public bzd::IChannel<T>
-{
-public:
-	virtual Bool empty() noexcept = 0;
-};
-
-	/// Advantage:
-	/// - Can pass a buffer to be written to.
-	/// - Compatible with IChannel.
-	///
-	/// - Function is not stateful
-	///        -> but class is.
-	IFiniteChannel readUntil(const T& value, filters...)
-	{
-	}
-
-	channel = readUntil(const T& value, filters...);
-	while (!channel.empty())
-	{
-		buffer = co_await channel.read(std::move(buffer));
-	}
-
-	for (const auto span : readUntil(...))
-	{
-	}
-*/
-
-	/// Read data until a specific value is found.
-	bzd::Async<bzd::Span<const T>> readUntil(const T& value) noexcept
-	{
-		auto readFromBuffer = buffer_.asSpanForReading();
-		if (readFromBuffer.empty())
+		/// Other solution, mixing both
+		auto readUntil(const T& value) noexcept
 		{
-			readFromBuffer = co_await !produce();
+			return [](Function<bzd::Span<T>(void)>) -> bzd::Generator<bzd::Span<const T>> {
+			};
 		}
-		const auto position = bzd::algorithm::find(readFromBuffer, value);
-		if (position == readFromBuffer.end())
-		{
-			co_return readFromBuffer;
-		}
-		co_return readFromBuffer.first(position);
-	}
 
-	/*    /// Read data until a specific sequence is found.
-		template <class Iterator>
-		requires concepts::forwardIterator<Iterator>
-		bzd::Async<bzd::Span<T>> readUntil(Iterator first, Iterator last) noexcept
+		auto factory = readUntil(...);
+		for (auto x : factory([&]() { return hello.subSpan(); }))
 		{
-			const auto readFromBuffer = buffer_.asSpanForReading();
+			x
+		}
+
+		/// Another
+		struct Context
+		{
+			Span read(Size)
+			{
+			}
+		};
+
+		readContext = makeReadContext(myString);
+
+		bzd::Generator<bzd::Span<const T>> readUntil(ContextReader context, const T& value) noexcept
+		{
+			data = context.allocate();
+			readFromBuffer = co_await in_.read(data);
+		}
+
+		bzd::Generator<bzd::Span<const T>> readCount(ContextReader context, const Size count) noexcept
+		{
+			bzd::ignore = co_await !context.read(in_, 12);
+		}
+
+		/// Or maybe the best is to:
+		/// 1. return bzd::Generator<bzd::Span<const T>>, because most of the functions need to write to the internal buffer anyway,
+		/// 2. for the few (only thinking about readCount) provide an overload that can write directly into a container.
+
+		template <class T>
+		class IFiniteChannel : public bzd::IChannel<T>
+		{
+		public:
+			virtual Bool empty() noexcept = 0;
+		};
+
+		/// Advantage:
+		/// - Can pass a buffer to be written to.
+		/// - Compatible with IChannel.
+		/// - Implementation can be splitted in different files.
+		///
+		/// - Function is not stateful
+		///        -> but class is.
+		/// - Function more complex and less efficient as it needs to start from the top all the time.
+		IFiniteChannel readUntil(const T& value, filters...)
+		{
+		}
+
+		channel = readUntil(const T& value, filters...);
+		while (!channel.empty())
+		{
+			buffer = co_await channel.read(std::move(buffer));
+		}
+
+		for (const auto span : readUntil(...))
+		{
 		}
 	*/
 
+	/// Read data until a specific value is found.
+	bzd::Generator<bzd::Span<const T>> readUntil(const T& value) noexcept
+	{
+		while (true)
+		{
+			if (auto readFromBuffer = buffer_.asSpanForReading(); !readFromBuffer.empty())
+			{
+				const auto position = bzd::algorithm::find(readFromBuffer, value);
+				if (position == readFromBuffer.end())
+				{
+					co_yield readFromBuffer;
+				}
+				else
+				{
+					buffer_.consume(position);
+					co_yield readFromBuffer.first(position);
+					break;
+				}
+			}
+			else if (auto readFromBuffer = co_await !read(); !readFromBuffer.empty())
+			{
+				const auto position = bzd::algorithm::find(readFromBuffer, value);
+				if (position == readFromBuffer.end())
+				{
+					co_yield readFromBuffer;
+				}
+				else
+				{
+					auto left = readFromBuffer.subSpan(position);
+					buffer_.assign(left);
+					co_yield readFromBuffer.first(position);
+					break;
+				}
+			}
+			else
+			{
+				co_await bzd::async::yield();
+			}
+		}
+	}
+
 private:
-	/// Produces new data in the ring buffer read from the input stream
-	/// and return what has been read.
-	[[nodiscard]] bzd::Async<bzd::Span<bzd::Byte>> produce() noexcept
+	/// Read new data from the input channel and return it.
+	/// This action does not produce data to the ring buffer.
+	[[nodiscard]] bzd::Async<bzd::Span<const bzd::Byte>> read() noexcept
 	{
 		auto span = buffer_.asSpanForWriting();
 		if (span.empty())
 		{
 			co_return bzd::error::Failure("Buffer of {} entries is full."_csv, buffer_.size());
 		}
-		auto result = co_await !in_.readToBuffer(span);
-		buffer_.produce(result.size());
-
-		co_return result;
+		co_return co_await !in_.read(span);
 	}
 
 private: // Variables.
