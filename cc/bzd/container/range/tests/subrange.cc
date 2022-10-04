75ca7eae2be06024f65fbd014f735b254f93eebf
blaizard <blaise.lengrand@gmail.com>
2022-10-04 22:01:02 +0200

    Adding tests to subRange

--- cc/bzd/container/range/tests/subrange.cc
+++ cc/bzd/container/range/tests/subrange.cc
@@ -2,11 +2,11 @@
 
 #include "cc/bzd/container/vector.hh"
 #include "cc/bzd/test/test.hh"
+#include "cc/bzd/test/types/range.hh"
 
 TEST(SubRange, Simple)
 {
 	bzd::Vector<bzd::UInt32, 12> vector;
-
 	bzd::range::SubRange emptyVectorView{vector.begin(), vector.end()};
 
 	static_assert(bzd::concepts::begin<decltype(emptyVectorView)>, "SubRange must validate the `begin` concept.");
@@ -23,4 +23,86 @@ TEST(SubRange, Simple)
 	bzd::range::SubRange nonEmptyVectorView{vector.begin(), vector.end()};
 	EXPECT_FALSE(nonEmptyVectorView.empty());
 	EXPECT_EQ(nonEmptyVectorView.size(), 3);
+}
+
+template <class T>
+concept hasSize = requires(T& t)
+{
+	t.size();
+};
+
+template <class T>
+concept hasData = requires(T& t)
+{
+	t.data();
+};
+
+template <class T>
+concept hasRandomAccessor = requires(T& t)
+{
+	t[0];
+};
+
+TEST(SubRange, VariousTypes)
+{
+	{
+		bzd::test::Range<bzd::typeTraits::InputTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(!bzd::concepts::sizedRange<decltype(subView)>, "No size member.");
+		static_assert(!hasSize<decltype(subView)>, "No size member.");
+		static_assert(!hasData<decltype(subView)>, "No data member.");
+		static_assert(!hasData<const decltype(subView)>, "No const data member.");
+		static_assert(!hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(!hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
+	{
+		bzd::test::Range<bzd::typeTraits::OutputTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(!bzd::concepts::sizedRange<decltype(subView)>, "No size member.");
+		static_assert(!hasSize<decltype(subView)>, "No size member.");
+		static_assert(!hasData<decltype(subView)>, "No data member.");
+		static_assert(!hasData<const decltype(subView)>, "No const data member.");
+		static_assert(!hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(!hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
+	{
+		bzd::test::Range<bzd::typeTraits::ForwardTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(bzd::concepts::sizedRange<decltype(subView)>, "Size member.");
+		static_assert(hasSize<decltype(subView)>, "Has size member.");
+		static_assert(!hasData<decltype(subView)>, "No data member.");
+		static_assert(!hasData<const decltype(subView)>, "No const data member.");
+		static_assert(!hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(!hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
+	{
+		bzd::test::Range<bzd::typeTraits::BidirectionalTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(bzd::concepts::sizedRange<decltype(subView)>, "Size member.");
+		static_assert(hasSize<decltype(subView)>, "Has size member.");
+		static_assert(!hasData<decltype(subView)>, "No data member.");
+		static_assert(!hasData<const decltype(subView)>, "No const data member.");
+		static_assert(!hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(!hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
+	{
+		bzd::test::Range<bzd::typeTraits::RandomAccessTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(bzd::concepts::sizedRange<decltype(subView)>, "Size member.");
+		static_assert(hasSize<decltype(subView)>, "Has size member.");
+		static_assert(!hasData<decltype(subView)>, "No data member.");
+		static_assert(!hasData<const decltype(subView)>, "No const data member.");
+		static_assert(hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
+	{
+		bzd::test::Range<bzd::typeTraits::ContiguousTag> range;
+		bzd::range::SubRange subView{range.begin(), range.end()};
+		static_assert(bzd::concepts::sizedRange<decltype(subView)>, "Size member.");
+		static_assert(hasSize<decltype(subView)>, "Has size member.");
+		static_assert(hasData<decltype(subView)>, "No data member.");
+		static_assert(hasData<const decltype(subView)>, "No const data member.");
+		static_assert(hasRandomAccessor<decltype(subView)>, "No random accessor.");
+		static_assert(hasRandomAccessor<const decltype(subView)>, "No const random accessor.");
+	}
 }
