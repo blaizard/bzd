c48b78c4b989b18cc76eb96b3138d7feef37663d
blaise <blaise.lengrand@gmail.com>
2022-10-04 22:03:22 +0200

    Renamed

--- cc/bzd/container/range/subrange.hh
+++ cc/bzd/container/range/subrange.hh
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "cc/bzd/container/range/view.hh"
+#include "cc/bzd/container/range/view_interface.hh"
 #include "cc/bzd/type_traits/sentinel_for.hh"
 
 namespace bzd::range {
 
 /// Create a sub range from a
 template <concepts::intputOrOutputIterator Iterator, concepts::sentinelFor<Iterator> Sentinel>
-class SubRange : public View<SubRange<Iterator, Sentinel>>
+class SubRange : public ViewInterface<SubRange<Iterator, Sentinel>>
 {
 public:
 	constexpr SubRange(const Iterator begin, const Sentinel end) noexcept : begin_{begin}, end_{end} {}
