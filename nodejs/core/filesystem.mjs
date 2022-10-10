6ad22b9b6b5fe5e2f9b6aa49f776ad59be4d785e
blaise <blaise.lengrand@gmail.com>
2022-10-10 22:08:39 +0200

    Resolving file type

--- nodejs/core/filesystem.mjs
+++ nodejs/core/filesystem.mjs
@@ -1,13 +1,9 @@
 import Path from "path";
 import Fs from "fs";
 
-/**
- * Collection of Promise-based asynchronous functions for the file system.
- */
+/// Collection of Promise-based asynchronous functions for the file system.
 export default class FileSystem {
-	/**
-	 * Checks if the file or directory exists
-	 */
+	/// Checks if the file or directory exists
 	static async exists(path) {
 		return new Promise((resolve) => {
 			Fs.access(path, Fs.F_OK, (e) => {
@@ -16,9 +12,7 @@ export default class FileSystem {
 		});
 	}
 
-	/**
-	 * Get the stat object associated with a file
-	 */
+	/// Get the stat object associated with a file
 	static async stat(path) {
 		return new Promise((resolve, reject) => {
 			Fs.stat(path, (e, stats) => {
@@ -27,18 +21,14 @@ export default class FileSystem {
 		});
 	}
 
-	/**
-	 * Create a directory recursively, if it already exists, do nothing
-	 */
+	/// Create a directory recursively, if it already exists, do nothing
 	static async mkdir(path) {
 		await Fs.promises.mkdir(path, {
 			recursive: true,
 		});
 	}
 
-	/**
-	 * Remove a directory recursively
-	 */
+	/// Remove a directory recursively
 	static async rmdir(path) {
 		const fileList = await FileSystem.readdir(path);
 		for (const i in fileList) {
@@ -55,9 +45,7 @@ export default class FileSystem {
 		await Fs.promises.rmdir(path);
 	}
 
-	/**
-	 * Read the content of a file
-	 */
+	/// Read the content of a file
 	static async readFile(path, options = "utf8") {
 		const data = await Fs.promises.readFile(path, {
 			encoding: options,
@@ -65,94 +53,75 @@ export default class FileSystem {
 		return data.toString();
 	}
 
-	/**
-	 * Write the content of a file
-	 */
+	/// Write the content of a file
 	static async writeFile(path, data, options = "utf8") {
 		await Fs.promises.writeFile(path, data, {
 			encoding: options,
 		});
 	}
 
-	/**
-	 * Read the content of a directory
-	 */
+	/// Read the content of a directory
 	static async readdir(path, withFileTypes = false) {
 		return await Fs.promises.readdir(path, { withFileTypes: withFileTypes });
 	}
 
-	/**
-	 * Move a file from a location to another. Note pathFrom and pathTo
-	 * are the full path of the file including the file name.
-	 */
+	/// Move a file from a location to another. Note pathFrom and pathTo
+	/// are the full path of the file including the file name.
 	static async move(pathFrom, pathTo) {
 		await Fs.promises.rename(pathFrom, pathTo);
 	}
 
-	/**
-	 * Rename a file.
-	 */
+	/// Rename a file.
 	static async rename(pathFrom, name) {
 		const pathTo = Path.join(Path.dirname(pathFrom), name);
 		await Fs.promises.rename(pathFrom, pathTo);
 	}
 
-	/**
-	 * Copy a file from a location to another. Note pathFrom and pathTo
-	 * are the full path of the file including the file name.
-	 * If the destination file already exists, it will fail.
-	 */
+	/// Copy a file from a location to another. Note pathFrom and pathTo
+	/// are the full path of the file including the file name.
+	/// If the destination file already exists, it will fail.
 	static async copy(pathFrom, pathTo) {
 		await Fs.promises.copyFile(pathFrom, pathTo, Fs.constants.COPYFILE_EXCL);
 	}
 
-	/**
-	 * Delete a file
-	 */
+	/// Delete a file
 	static async unlink(path) {
 		await Fs.promises.unlink(path);
 	}
 
-	/**
-	 * Append data to a file
-	 */
+	/// Append data to a file
 	static async appendFile(path, data) {
 		await Fs.promises.appendFile(path, data);
 	}
 
-	/**
-	 * Truncate a file to a specific size
-	 */
+	/// Truncate a file to a specific size
 	static async truncate(path, fileSize) {
 		await Fs.promises.truncate(path, fileSize);
 	}
 
-	/**
-	 * Touch a file (create it if it does not exists) and
-	 * updates its last modification date.
-	 */
+	/// Touch a file (create it if it does not exists) and
+	/// updates its last modification date.
 	static async touch(path) {
 		const fd = await FileSystem.open(path, "a");
 		await FileSystem.close(fd);
 	}
 
-	/**
-	 * Open a file asynchronously
-	 */
+	/// Open a file asynchronously
 	static async open(path, options) {
 		return await Fs.promises.open(path, options);
 	}
 
-	/**
-	 * Close a file asynchronously, previously open with "open"
-	 */
+	/// Close a file asynchronously, previously open with "open"
 	static async close(fileHandle) {
 		await fileHandle.close();
 	}
 
-	/**
-	 * Walk through a directory and print all files
-	 */
+	/// Determines the actual location of a path and resolve symlinks.
+	static async realpath(path, options) {
+		return await Fs.promises.realpath(path, options);
+	}
+
+	/// Walk through a directory and print all files
 	static async *walk(path) {
 		const dirents = await Fs.promises.readdir(path, { withFileTypes: true });
 		for (const dirent of dirents) {
