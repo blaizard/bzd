467397a5386edb26923fc0be111dd5f712867a23
blaise <blaise.lengrand@gmail.com>
2022-12-05 22:05:47 +0100

    Adding serialization

--- cc/bzd/core/serialization/tests/integral.cc
+++ cc/bzd/core/serialization/tests/integral.cc
@@ -5,7 +5,7 @@
 
 TEST(Integral, Boolean)
 {
-	bzd::String<128u> string;
+	bzd::String<20u> string;
 
 	{
 		const auto size = bzd::serialize(string, true);
@@ -19,5 +19,20 @@ TEST(Integral, Boolean)
 		EXPECT_EQ(size, 1u);
 		EXPECT_EQ(string[1u], '\x00');
 		EXPECT_EQ(string.size(), 2u);
+	}
+}
+
+TEST(Integral, UInt32)
+{
+	bzd::String<20u> string;
+
+	{
+		const auto size = bzd::serialize(string, bzd::UInt32{0x12345678});
+		EXPECT_EQ(size, 4u);
+		EXPECT_EQ(string[0], '\x78');
+		EXPECT_EQ(string[1], '\x56');
+		EXPECT_EQ(string[2], '\x34');
+		EXPECT_EQ(string[3], '\x12');
+		EXPECT_EQ(string.size(), 4u);
 	}
 }
