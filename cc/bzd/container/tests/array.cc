bc874ac3d8df8a61454ff3c07a964484ab923a14
blaise <blaise.lengrand@gmail.com>
2023-01-03 10:13:32 +0100

    Cleanup

--- cc/bzd/container/tests/array.cc
+++ cc/bzd/container/tests/array.cc
@@ -11,6 +11,15 @@ TEST(ContainerArray, Base)
 	EXPECT_EQ(array[2], 4);
 }
 
+TEST(ContainerArray, Reference)
+{
+	int a = 1;
+	bzd::Array<int&, 1> array{bzd::inPlace, a};
+	EXPECT_EQ(array[0], 1);
+	a = 2;
+	EXPECT_EQ(array[0], 2);
+}
+
 TEST_CONSTEXPR_BEGIN(ContainerArray, Constexpr)
 {
 	constexpr bzd::Array<int, 5> array{};
