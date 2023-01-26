df55e08592c905970bf3ae9ed3fdb1741a2898a2
blaizard <blaise.lengrand@gmail.com>
2023-01-26 21:18:48 +0100

    Adding documentation

--- sanitize.sh
+++ sanitize.sh
@@ -7,9 +7,3 @@ WORKSPACE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 # Sanitize
 ./tools/bazel run //tools/sanitizer:formatter
 ./tools/bazel run //tools/sanitizer:analyzer
-
-# Generate documentation
-rm -rfd "$WORKSPACE_PATH/docs/api"
-mkdir "$WORKSPACE_PATH/docs/api" "$WORKSPACE_PATH/docs/api/bazel"
-
-./tools/bazel run //tools/documentation/bazel -- "$WORKSPACE_PATH/docs/api/bazel"
