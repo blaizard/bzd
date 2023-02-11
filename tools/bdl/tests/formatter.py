d96dc7ccd87e5d97b45625a46cec6c203516b5eb
blaise <blaise.lengrand@gmail.com>
2023-02-11 12:28:00 +0100

    Fixed formatter

--- tools/bdl/tests/formatter.py
+++ tools/bdl/tests/formatter.py
@@ -8,6 +8,9 @@ from tools.bdl.generators.bdl.visitor import formatBdl
 
 class TestRun(unittest.TestCase):
 
+	def _stripString(self, content: str) -> typing.List[str]:
+		return [line.strip() for line in content.split("\n") if line.strip()]
+
 	def testFormatter(self) -> None:
 
 		bdl = Object.fromContent(content="""/*
@@ -22,25 +25,39 @@ complex = int32<Int, List<T<A/*Variable A*/, B, C<UInt>>>>;
 }
 
 interface MyFy {
-                 var     =MyType;
+		     var     =MyType;
 
 /*
-          A nested comment
-         */
+	    A nested comment
+	   */
 
-        varConst = const MyType<T> [ /*    Immer */
-        integer ];
+	  varConst = const MyType<T> [ /*    Immer */
+	  integer ];
 varInitialized= MyType(   42 );    
 
 
-      method myMethod() -> Integer;
+	method myMethod() -> Integer;
 }
 """)
 		output = formatBdl(bdl)
 
-		self.assertRegex(output, r"interface MyFy")
-		self.assertRegex(output, r"method myMethod")
+		self.assertEqual(self._stripString(output), self._stripString("""
+// This is a multi-line comment
+struct ab {
+	// Contracts
+	defaultConstant = const int32 [min(-1) max(35)];
+	complex = int32<Int, List<T</*Variable A*/ A, B, C<UInt>>>>;
 
+}
+
+interface MyFy {
+	var = MyType;
+	// 	    A nested comment
+	varConst = const MyType<T> [/*Immer*/integer];
+	varInitialized = MyType(42);
+	method myMethod() -> Integer;
+
+}"""))
 
 if __name__ == '__main__':
 	unittest.main()
