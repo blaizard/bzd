3ea87983e499859a81405ccd5d77b6ec67c719bc
blaizard <blaise.lengrand@gmail.com>
2023-04-26 08:39:58 +0200

    Adding Optional<Size> to serialize

--- cc/bzd/core/serialization/tests/floating_point.cc
+++ cc/bzd/core/serialization/tests/floating_point.cc
@@ -8,7 +8,8 @@ TEST(FloatingPoint, Float32)
 
 	{
 		const auto result = bzd::serialize(string.assigner(), bzd::Float32{3.14});
-		EXPECT_EQ(result, 4u);
+		EXPECT_TRUE(result);
+		EXPECT_EQ(result.value(), 4u);
 		EXPECT_EQ(string.size(), 4u);
 		EXPECT_STREQ(string.data(), "\xc3\xf5\x48\x40");
 	}
@@ -24,7 +25,7 @@ TEST(FloatingPoint, Float32)
 	{
 		bzd::String<1u> shortString;
 		const auto result1 = bzd::serialize(shortString.assigner(), bzd::Float32{3.14});
-		EXPECT_EQ(result1, 1u);
+		EXPECT_FALSE(result1);
 		EXPECT_EQ(shortString.size(), 1u);
 		EXPECT_STREQ(shortString.data(), "\xc3");
 
@@ -40,7 +41,8 @@ TEST(FloatingPoint, Float64)
 
 	{
 		const auto result = bzd::serialize(string.assigner(), bzd::Float64{3.14});
-		EXPECT_EQ(result, 8u);
+		EXPECT_TRUE(result);
+		EXPECT_EQ(result.value(), 8u);
 		EXPECT_EQ(string.size(), 8u);
 		EXPECT_STREQ(string.data(), "\x1f\x85\xeb\x51\xb8\x1e\x09\x40");
 	}
@@ -56,7 +58,7 @@ TEST(FloatingPoint, Float64)
 	{
 		bzd::String<1u> shortString;
 		const auto result1 = bzd::serialize(shortString.assigner(), bzd::Float64{3.14});
-		EXPECT_EQ(result1, 1u);
+		EXPECT_FALSE(result1);
 		EXPECT_EQ(shortString.size(), 1u);
 		EXPECT_STREQ(shortString.data(), "\x1f");
 
