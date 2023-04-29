504f95250e5c8be1e48e64ee5e8ff7cf7961e432
blaise <blaise.lengrand@gmail.com>
2023-04-29 16:58:19 +0200

    Fix for clang-tidy

--- cc/bzd/utility/pattern/reader/integral.hh
+++ cc/bzd/utility/pattern/reader/integral.hh
@@ -18,11 +18,11 @@ template <concepts::integral T>
 struct Formatter<T>
 {
 private:
-	static constexpr Array<const char, 16> digits_{inPlace, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	static constexpr Array<const char, 16> digitsPredefined{inPlace, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 public:
-	template <concepts::inputStreamRange Range, Size base = 10, class Digits = decltype(digits_)>
-	static constexpr Optional<Size> fromString(Range&& range, T& data, const Digits& digits = digits_) noexcept
+	template <concepts::inputStreamRange Range, Size base = 10, class Digits = decltype(digitsPredefined)>
+	static constexpr Optional<Size> fromString(Range&& range, T& data, const Digits& digits = digitsPredefined) noexcept
 	{
 		static_assert(base > 1 && base <= 16, "Invalid base size.");
 		static_assert(Digits::size() >= base, "There is not enough digits for the base.");
