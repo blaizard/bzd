fc948ef3f3a8c33c00999aa40fd2335bafe775b4
blaizard <blaise.lengrand@gmail.com>
2023-06-17 12:23:53 +0200

    Fixed python files

--- toolchains/python/linux_x86_64/python_test.py
+++ toolchains/python/linux_x86_64/python_test.py
@@ -6,7 +6,7 @@ class PythonTest(unittest.TestCase):
 
 	def test(self):
 		self.assertEqual(sys.version_info, (3, 8, 16, "final", 0))
-		self.assertIn("python3_linux_x86_64/bin/python3.8", sys.executable)
+		self.assertIn("python3_linux_x86_64/bin/python3", sys.executable)
 
 
 if __name__ == '__main__':
