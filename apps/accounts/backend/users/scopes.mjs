a2853c5a135d77e519c76c480f37a2af096ca2ba
blaise <blaise.lengrand@gmail.com>
2023-11-14 21:05:56 +0100

    Make scopes as exports

---
+++ apps/accounts/backend/users/scopes.mjs
@@ -0,0 +1,20 @@
+/// Read anything related to users.
+export const scopeAdminUserRead = "admin-users:r";
+/// Change anything related to users, including creating/deleting users.
+export const scopeAdminUserWrite = "admin-users:w";
+/// Read basic information about the current user.
+export const scopeSelfBasicRead = "self-basic:r";
+/// Change basic information about the current user.
+export const scopeSelfBasicWrite = "self-basic:w";
+/// Read the current user roles.
+export const scopeSelfRolesRead = "self-roles:r";
+/// Update the current user roles.
+export const scopeSelfRolesWrite = "self-roles:w";
+/// Read the current user tokens.
+export const scopeSelfTokensRead = "self-tokens:r";
+/// Change the current user tokens.
+export const scopeSelfTokensWrite = "self-tokens:w";
+/// Read the current user subscriptions.
+export const scopeSelfSubscriptionsRead = "self-subscriptions:r";
+/// Change the current user subscriptions.
+export const scopeSelfSubscriptionsWrite = "self-subscriptions:w";
