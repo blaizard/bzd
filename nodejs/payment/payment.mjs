192cd6bfd7ea34e53adecd8f22c49cc359cc43d4
blaise <me@example.com>
2024-01-22 18:50:36 +0100

    Adding payment hooks

--- nodejs/payment/payment.mjs
+++ nodejs/payment/payment.mjs
@@ -1,19 +1,39 @@
+import ExceptionFactory from "#bzd/nodejs/core/exception.mjs";
+
+const Exception = ExceptionFactory("payment");
+
 export class Subscription {
-	constructor(uid, end) {
+	constructor(uid, timestampEndMs = null) {
 		this.uid = uid;
-		this.end = end;
+		this.timestampEndMs = timestampEndMs;
 	}
 }
 
 export class PaymentInterface {
 	constructor(callbackPayment) {
-		/// callbackPayment(uid, email, products, maybeSubscription: Subscription = null)
-		///
-		/// \param uid The unique identifier corresponding to this payment.
-		/// \param products The list of products associated with this payment. All products
-		///                 have a 'uid' field which is a unique identifier for the product
-		///                 at the payment provider side.
-		/// \return true if it was processed, false otherwise.
-		this.callbackPayment = callbackPayment;
+		this._callbackPayment = callbackPayment;
+	}
+
+	/// callbackPayment(uid, email, products, maybeSubscription: Subscription = null)
+	///
+	/// \param uid The unique identifier corresponding to this payment.
+	/// \param products The list of products associated with this payment. All products
+	///                 have a 'uid' field which is a unique identifier for the product
+	///                 at the payment provider side.
+	/// \return true if it was processed, false otherwise.
+	async callbackPayment(uid, email, products, maybeSubscription = null) {
+		try {
+			Exception.assert(products.length > 0, "There must be at least one product: {}", products);
+			return await this._callbackPayment(uid, email, products, maybeSubscription);
+		} catch (e) {
+			Exception.fromError(e).print(
+				"Error with callbackPayment({}, {}, {}, {})",
+				uid,
+				email,
+				JSON.stringify(products),
+				maybeSubscription,
+			);
+			throw e;
+		}
 	}
 }
