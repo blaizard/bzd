9ff29c9a9301d2b304c9966dfc46c6215d14df85
blaise <me@example.com>
2024-03-10 14:57:27 +0100

    Accept empty plugins

--- nodejs/vue/rest.mjs
+++ nodejs/vue/rest.mjs
@@ -2,6 +2,12 @@ import Rest from "../core/rest/client.mjs";
 
 export default {
 	install(app, options) {
+		options = Object.assign(
+			{
+				plugins: [],
+			},
+			options,
+		);
 		const rest = new Rest(options.schema, options);
 		rest.installPlugins(...options.plugins).catch((e) => console.error(e));
 		app.config.globalProperties.$rest = rest;
