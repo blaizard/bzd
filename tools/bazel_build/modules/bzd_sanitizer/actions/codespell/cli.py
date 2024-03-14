3a26f2b33ed53f9ce95376ff635738a1d52b30b7
blaise <blaise.lengrand@gmail.com>
2024-03-14 18:44:58 +0100

    Adding codespell

---
+++ tools/bazel_build/modules/bzd_sanitizer/actions/codespell/cli.py
@@ -0,0 +1,42 @@
+import argparse
+import pathlib
+import typing
+
+from bzd_sanitizer.worker import worker
+from bzd.utils.run import localBazelBinary
+
+
+def workload(args: typing.Tuple[pathlib.Path, pathlib.Path, bool, str, str], stdout: typing.TextIO) -> bool:
+	workspace, path, check, codespell, config = args
+
+	params = []
+	#params += ["--check"] if check else []
+	#params += ["--config", config] if check else []
+
+	result = localBazelBinary(
+	    codespell,
+	    args=params + [str(workspace / path)],
+	    ignoreFailure=True,
+	    stdout=stdout,
+	    stderr=stdout,
+	)
+
+	return result.isSuccess()
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Codespell.")
+	parser.add_argument("--codespell", type=pathlib.Path, help="The path of the codespell binary.")
+	parser.add_argument(
+	    "--config",
+	    type=pathlib.Path,
+	    help="The path of the configuration file for codespell.",
+	)
+	parser.add_argument("context", type=pathlib.Path, help="The context file path.")
+	args = parser.parse_args()
+
+	worker(
+	    args.context,
+	    workload,
+	    args=[str(args.codespell), str(args.config)],
+	)
