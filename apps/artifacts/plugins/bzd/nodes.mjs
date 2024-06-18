c408a6d20c7808fa23ad6c8a943f97c7df66cff3
blaizard <blaise.lengrand@gmail.com>
2024-06-18 18:50:03 +0200

    Improving node

--- apps/artifacts/plugins/bzd/nodes.mjs
+++ apps/artifacts/plugins/bzd/nodes.mjs
@@ -2,18 +2,46 @@ import ExceptionFactory from "#bzd/nodejs/core/exception.mjs";
 
 const Exception = ExceptionFactory("apps", "plugin", "bzd");
 
+/// A node is a collection of data.
+///
+/// Data are denominated by a path, each data can be set at various interval.
+/// Data have a validity time that is adjusted automatically.
 class Node {
 	constructor() {
 		this.data = {};
 		this.pending = [];
-		this.timestamp = null;
+	}
+
+	/// Identify the path of the fragments that are being set.
+	///
+	/// A path is determined by the full path up to a value or a list.
+	///
+	/// \return A dictionary of paths and values. Each path is a string which segments are joint with dots.
+	getAllPathAndValues(fragment) {
+		let paths = {};
+		for (const [key, value] of Object.entries(fragment)) {
+			if (value && value.constructor == Object) {
+				for (const [subKey, subValue] of Object.entries(this.getAllPathAndValues(value))) {
+					paths[key + "." + subKey] = subValue;
+				}
+			} else {
+				paths[key] = value;
+			}
+		}
+		return paths;
 	}
 
 	async insert(category, fragment) {
 		const timestamp = Date.now();
 		let data = await this.get(category);
-		Object.assign(data, fragment);
-		this.timestamp = timestamp;
+
+		// Identify the path of the fragments and their values.
+		for (const [path, value] in Object.entries(this.getAllPathAndValues(fragment))) {
+			data[path] = {
+				timestamp: timestamp,
+				value: value,
+			};
+		}
 	}
 
 	async getCategories() {
