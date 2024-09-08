0a6c77318190e2b071eafc6237f92e893e5025b9
blaizard <blaise.lengrand@gmail.com>
2024-09-08 17:33:27 +0200

    Use different format stamp

--- tools/bazel_build/buildstamp/stamp.sh
+++ tools/bazel_build/buildstamp/stamp.sh
@@ -3,7 +3,7 @@
 set -eu
 
 REVISION=$(git rev-parse HEAD | tr -d '\n\r')
-TIMESTAMP=$(git show --no-patch --format=%ct ${REVISION})
+TIMESTAMP=$(git show --no-patch --format=%cd --date=format:%Y%m%d-%H%M%S ${REVISION})
 NB_DIFFS=$(git status --porcelain 2>/dev/null | wc -l)
 VERSION="${TIMESTAMP}-${REVISION}"
 if [ "$NB_DIFFS" -ne "0" ]; then
