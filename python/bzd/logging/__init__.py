bc4a242aa55486273ec9ad055347f4d5910ab7ac
blaise <blaise@email.com>
2024-09-27 10:49:47 +0200

    Adding handlersScope to logger

--- python/bzd/logging/__init__.py
+++ python/bzd/logging/__init__.py
@@ -1,8 +1,9 @@
 import logging
 import typing
 import time
+from contextlib import contextmanager
 
-from bzd.logging.handler import Log, LoggerHandler, LoggerHandlerFlow
+from bzd.logging.handler import Log, LoggerHandler, LoggerHandlerScope, LoggerHandlerFlow, LoggerHandlerCallback
 
 # Default logger
 logging.basicConfig(
@@ -27,16 +28,37 @@ class Logger:
 
 	def __init__(self, name: str) -> None:
 		self.logger = logging.getLogger(name)
-		self._handlers: typing.List[typing.List[LoggerHandler]] = []
+		self._handlers: typing.List[typing.List[LoggerHandlerCallback]] = []
 		self.logger.addHandler(_CallbackHandler(self._callback))
 
 	def handlers(self, *handlers: LoggerHandler) -> "Logger":
 		"""Register handlers to the current logger, this action disables the default backend."""
 
 		self.logger.propagate = False
-		self._handlers.append([*handlers])
+		self._handlers.append([handler.handler for handler in handlers])
 
 		return self
+
+	@contextmanager
+	def handlersScope(self, *handlers: typing.Union[LoggerHandler,
+	                                                LoggerHandlerScope]) -> typing.Generator[None, None, None]:
+		"""Register scoped handlers."""
+
+		for handler in handlers:
+			if isinstance(handler, LoggerHandlerScope):
+				handler.constructor()
+
+		index = len(self._handlers)
+		self.logger.propagate = False
+		self._handlers.append([handler.handler for handler in handlers])
+
+		yield
+
+		self._handlers.pop(index)
+
+		for handler in handlers:
+			if isinstance(handler, LoggerHandlerScope):
+				handler.destructor()
 
 	def _callback(self, log: Log) -> None:
 		"""Calls all handlers sequentially."""
