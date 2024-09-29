7711414e7564216b717f9c8b53bf22b6ab080194
blaise <blaise.lengrand@gmail.com>
2024-09-29 17:19:06 +0200

    Default log to stderr

--- python/bzd/logging/handler/stderr.py
+++ python/bzd/logging/handler/stderr.py
@@ -1,10 +1,11 @@
+import sys
 from datetime import datetime
 
 from bzd.logging.handler import Log, LoggerHandler, LoggerHandlerData, LoggerHandlerFlow
 
 
-class LoggerHandlerStdout(LoggerHandler):
-	"""Print to stdout."""
+class LoggerHandlerStderr(LoggerHandler):
+	"""Print to stderr."""
 
 	def format(self, log: Log) -> str:
 		return f"[{datetime.fromtimestamp(log.timestamp)}] [{log.level}] [{log.name}] [{log.filename}:{log.line}] {log.message}"
@@ -12,6 +13,7 @@ class LoggerHandlerStdout(LoggerHandler):
 	def handler(self, data: LoggerHandlerData, flow: LoggerHandlerFlow) -> None:
 
 		for log in data:
-			print(self.format(log))
+			print(self.format(log), file=sys.stderr)
+		sys.stderr.flush()
 
 		flow.next(data=data)
