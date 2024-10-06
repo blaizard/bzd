ccefdf36524bdccba54dbf952c51843cce10a245
blaise <blaise@email.com>
2024-10-06 05:57:28 +0200

    Adding pathList

--- apps/artifacts/plugins/nodes/view_category.vue
+++ apps/artifacts/plugins/nodes/view_category.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="data" v-if="metadata">
 		<div>Updated: {{ durationString }} ago</div>
-		<Value :value="processedData"></Value>
+		<Value :value="processedData" :path-list="pathList"></Value>
 	</div>
 </template>
 
