ef7e09f6b66e5392eadf19805a215d50920e8037
blaise <blaise@email.com>
2024-10-07 03:43:44 +0200

    Adding value component

---
+++ apps/artifacts/plugins/nodes/serialize.vue
@@ -0,0 +1,92 @@
+<template>
+	<div class="serialize-container">
+		<template v-if="isObject"
+			>{<br />
+			<div v-for="(subValue, key) in value" class="indent">
+				<span>{{ key }}</span
+				>:
+				<Serialize :value="subValue" :path-list="pathListAppendKey(key)" v-slot="slotProps">
+					<slot :value="slotProps.value" :path-list="slotProps.pathList"></slot> </Serialize
+				>,
+			</div>
+			}</template
+		>
+		<template v-else-if="isArray">
+			<template v-if="isPathList">
+				<slot :value="value" :path-list="pathList"></slot>
+			</template>
+			<template v-else>
+				[
+				<div v-for="subValue in value" class="inline">
+					<Serialize :value="subValue" :path-list="pathList" v-slot="slotProps">
+						<slot :value="slotProps.value" :path-list="slotProps.pathList"></slot> </Serialize
+					>,
+				</div>
+				]</template
+			>
+		</template>
+		<template v-else>
+			<div class="inline">
+				{{ valueToString(value) }}
+			</div>
+		</template>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Serialize",
+		props: {
+			value: { mandatory: true },
+			pathList: { mandatory: false, default: null, type: Array || null },
+		},
+		computed: {
+			isObject() {
+				return !!this.value && this.value.constructor === Object;
+			},
+			isArray() {
+				return !!this.value && this.value.constructor === Array;
+			},
+			isString() {
+				return typeof this.value === "string";
+			},
+			isNull() {
+				return this.value === null;
+			},
+			isPathList() {
+				return this.pathList !== null;
+			},
+		},
+		methods: {
+			pathListAppendKey(key) {
+				if (this.pathList === null) {
+					return null;
+				}
+				return [...this.pathList, key];
+			},
+			valueToString(value) {
+				if (typeof value === "string") {
+					return '"' + value + '"';
+				}
+				if (value === null) {
+					return "null";
+				}
+				return value.toString();
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.serialize-container {
+		display: inline;
+
+		.inline {
+			display: inline;
+		}
+
+		.indent {
+			padding-left: 20px;
+		}
+	}
+</style>
--- apps/artifacts/plugins/nodes/value.vue
+++ apps/artifacts/plugins/nodes/value.vue
@@ -1,58 +1,24 @@
 <template>
 	<div class="value-container">
-		<template v-if="isObject"
-			>{<br />
-			<div v-for="(subValue, key) in value" class="indent">
-				<span>{{ key }}</span
-				>: <Value :value="subValue" :path-list="[...pathList, key]"></Value>,
-			</div>
-			}</template
-		>
-		<template v-else-if="isArray"
-			>[
-			<div v-for="subValue in value" class="inline"><Value :value="subValue" :path-list="pathList"></Value>,</div>
-			]</template
-		>
-		<template v-else>
-			<div class="inline">
-				{{ valueToString(value) }}
-			</div>
-		</template>
+		<div v-for="[t, v] in valueTimestampList" class="inline">
+			<span>{{ t }}</span
+			><code><slot class="value" :value="v"></slot></code>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "Value",
 		props: {
-			value: { mandatory: false },
-			pathList: { mandatory: false, type: Array },
+			value: { mandatory: true },
+			pathList: { mandatory: true, type: Array },
 		},
 		computed: {
-			isObject() {
-				return !!this.value && this.value.constructor === Object;
-			},
-			isArray() {
-				return !!this.value && this.value.constructor === Array;
-			},
-			isString() {
-				return typeof this.value === "string";
-			},
-			isNull() {
-				return this.value === null;
+			valueTimestampList() {
+				return [this.value];
 			},
 		},
-		methods: {
-			valueToString(value) {
-				if (typeof value === "string") {
-					return '"' + value + '"';
-				}
-				if (value === null) {
-					return "null";
-				}
-				return value.toString();
-			},
-		},
+		methods: {},
 	};
 </script>
 
@@ -64,8 +30,11 @@
 			display: inline;
 		}
 
-		.indent {
-			padding-left: 20px;
+		.value {
+			background-color: #ccc;
+			border: solid 1px rgb(229.5, 229.5, 229.5);
+			border-radius: 6px;
+			padding: 2px 5px;
 		}
 	}
 </style>
