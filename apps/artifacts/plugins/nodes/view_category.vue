1acef0a7abac21ec4822291fc41f399ad4d050c8
blaise <blaise.lengrand@gmail.com>
2024-10-09 11:47:35 +0200

    Adding value accessor

--- apps/artifacts/plugins/nodes/view_category.vue
+++ apps/artifacts/plugins/nodes/view_category.vue
@@ -7,13 +7,17 @@
 			</Value>
 		</Serialize>
 		<div>
-			Accessors:
+			<span v-if="isValue(value)">Accessors for {{ pathList.join("/") }}:</span>
+			<span v-else>Accessors:</span>
 			<ul>
 				<li>
 					<a :href="endpoint">{{ endpoint }}</a>
 				</li>
 				<li>
 					<a :href="endpoint + '?metadata=1'">{{ endpoint }}?metadata=1</a>
+				</li>
+				<li v-if="isValue(value)">
+					<a :href="endpoint + '?count=5'">{{ endpoint }}?count=5</a>
 				</li>
 			</ul>
 		</div>
@@ -59,6 +63,9 @@
 			endpoint() {
 				return "/x/" + this.pathList.map(encodeURIComponent).join("/");
 			},
+			value() {
+				return this.metadata.data || {};
+			},
 		},
 		mounted() {
 			this.fetchMetadata();
@@ -67,13 +74,15 @@
 			clearTimeout(this.timeout);
 		},
 		methods: {
+			isValue(value) {
+				return Array.isArray(value);
+			},
 			/// Iterate through the metadata and return a copy of it.
 			iterateMetadata(callback) {
-				const isValue = (data) => Array.isArray(data) && data.length == 2;
 				const recursive = (object, callback) => {
 					let updatedObject = {};
 					Object.entries(object).forEach(([key, data]) => {
-						if (isValue(data)) {
+						if (this.isValue(data)) {
 							updatedObject[key] = callback(data[0], data[1]);
 						} else if (data && typeof data === "object") {
 							updatedObject[key] = recursive(data, callback);
@@ -81,11 +90,10 @@
 					});
 					return updatedObject;
 				};
-				const object = this.metadata.data;
-				if (isValue(object)) {
-					return callback(object[0], object[1]);
+				if (this.isValue(this.value)) {
+					return callback(this.value[0], this.value[1]);
 				}
-				return recursive(object, callback);
+				return recursive(this.value, callback);
 			},
 			async fetchMetadata() {
 				await this.handleSubmit(async () => {
