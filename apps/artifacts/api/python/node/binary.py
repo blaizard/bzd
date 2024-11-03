86149890a8ede4702b697afebf16cf5d0663f46d
blaise <blaise@email.com>
2024-11-03 08:15:27 +0100

    Refactored node binary

--- apps/artifacts/api/python/node/binary.py
+++ apps/artifacts/api/python/node/binary.py
@@ -3,45 +3,73 @@ import typing
 import time
 import subprocess
 import os
+import sys
 
-from apps.artifacts.api.python.node.node import Node
-
-
-def cleanPathToList(data: str) -> typing.List[str]:
-	"""CLeanup the given path."""
-
-	segments: typing.List[str] = []
-	for segment in data.split("/"):
-		segment = segment.strip()
-		if not segment or segment == ".":
-			continue
-		if segment == "..":
-			assert len(segments) > 0, f"The path '{data}' is going above the root."
-			segments.pop()
-		else:
-			segments.append(segment)
-	return segments
+from apps.artifacts.api.python.node.node import Node, NodePublishNoRemote
+from bzd.logging import Logger
 
 
-def publish(node: Node, args: argparse.Namespace) -> None:
-	"""Publish some data to a remote node."""
+class Utility:
 
-	pathData = cleanPathToList(args.data)
-	assert len(pathData) > 0, f"There is no data to publish, the data given '{args.data}' evaluates to '.'"
-	node.publish(data=pathData[-1], path=pathData[:-1])
-	print(f"Published to node {args.uid}:{'/'.join(pathData[:-1])}: {pathData[-1]}")
+	def __init__(self, uid: str, logger: Logger) -> None:
+		self.uid = uid
+		self.node = Node(uid=self.uid, logger=logger)
+		self.logger = logger
 
+	def fail(self, message: str, errorCode: int = 1) -> None:
+		self.logger.error(message)
+		sys.exit(errorCode)
 
-def timeit(node: Node, args: argparse.Namespace) -> None:
-	"""Time the given command and publish it."""
+	def assertTrue(self, condition: bool, message: str, errorCode: int = 1) -> None:
+		if not condition:
+			self.fail(message, errorCode)
 
-	timeStart = time.time()
-	subprocess.run(args.rest, check=True, cwd=os.environ.get("BUILD_WORKSPACE_DIRECTORY", None))
-	duration = time.time() - timeStart
+	def cleanPathToList(self, data: str) -> typing.List[str]:
+		"""CLeanup the given path."""
 
-	path = cleanPathToList(args.path)
-	node.publish(path=path, data=duration)
-	print(f"Time elapsed {duration}s published to node {args.uid}:{'/'.join(path)}")
+		segments: typing.List[str] = []
+		for segment in data.split("/"):
+			segment = segment.strip()
+			if not segment or segment == ".":
+				continue
+			if segment == "..":
+				self.assertTrue(len(segments) > 0, f"The path '{data}' is going above the root.")
+				segments.pop()
+			else:
+				segments.append(segment)
+		return segments
+
+	def _publish(self, path: typing.List[str], data: typing.Any) -> None:
+		"""Publish to a remote and ignore failure if any."""
+
+		try:
+			self.node.publish(data=data, path=path)
+			self.logger.info(f"Published to node {self.uid}:{'/'.join(path)}: {data}")
+
+		except NodePublishNoRemote as e:
+			self.logger.error(str(e))
+
+	def publish(self, data: str) -> None:
+		"""Publish some data to a remote node."""
+
+		pathData = self.cleanPathToList(data)
+		self.assertTrue(len(pathData) > 0, f"There is no data to publish, the data given '{data}' evaluates to '.'")
+		self._publish(data=pathData[-1], path=pathData[:-1])
+
+	def timeit(self, path: str, args: typing.List[str]) -> None:
+		"""Time the given command and publish it."""
+
+		timeStart = time.time()
+
+		result = subprocess.run(args, cwd=os.environ.get("BUILD_WORKSPACE_DIRECTORY", None))
+		self.assertTrue(result.returncode == 0, f"Process failed with return code {result.returncode}",
+		                result.returncode)
+
+		duration = time.time() - timeStart
+
+		pathAsList = self.cleanPathToList(path)
+		self._publish(path=pathAsList, data=duration)
+		self.logger.info(f"Time elapsed {duration}s")
 
 
 if __name__ == "__main__":
@@ -58,11 +86,12 @@ if __name__ == "__main__":
 
 	args = parser.parse_args()
 
-	node = Node(uid=args.uid)
+	logger = Logger("node")
+	utility = Utility(uid=args.uid, logger=logger)
 
 	if args.command == "publish":
-		publish(node=node, args=args)
+		utility.publish(data=args.data)
 	elif args.command == "timeit":
-		timeit(node=node, args=args)
+		utility.timeit(args=args.rest, path=args.path)
 	else:
 		raise NotImplementedError(f"Command '{args.command}' is not implemented.")
