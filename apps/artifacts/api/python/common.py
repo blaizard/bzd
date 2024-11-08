408400333ac3f11fe14ae3716882a2e3d91aeb42
blaise <blaise.lengrand@gmail.com>
2024-11-08 07:32:00 +0100

    Make use of BZD_NODE_TOKEN

--- apps/artifacts/api/python/common.py
+++ apps/artifacts/api/python/common.py
@@ -1,18 +1,22 @@
 import typing
+import os
 
-from apps.artifacts.api.config import remotes
+from apps.artifacts.api.config import remotes as configRemotes, token as configToken
 from bzd.logging import Logger
 
-assert len(remotes) > 0, f"'remotes' from the API config cannot be empty."
+assert len(configRemotes) > 0, f"'remotes' from the API config cannot be empty."
 
 
 class ArtifactsBase:
 	"""Common libraries for the Artifacts API library."""
 
-	def __init__(self,
-	             uid: typing.Optional[str] = None,
-	             token: typing.Optional[str] = None,
-	             logger: Logger = Logger("artifacts.api")) -> None:
+	def __init__(
+	    self,
+	    uid: typing.Optional[str] = None,
+	    remotes: typing.List[str] = configRemotes,
+	    token: typing.Optional[str] = os.environ.get("BZD_NODE_TOKEN", configToken),
+	    logger: Logger = Logger("artifacts.api")
+	) -> None:
 		"""Construct the Artifacts API object.
 
 		Args:
@@ -22,6 +26,7 @@ class ArtifactsBase:
 		"""
 
 		self.uid = uid
+		self.remoteSources = remotes
 		self.token = token
 		self.logger = logger
 		self.remote: typing.Optional[str] = None
@@ -39,7 +44,7 @@ class ArtifactsBase:
 				yield self.remote, i, 2
 
 		# No valid remote, test the potential ones.
-		for remote in remotes:
+		for remote in self.remoteSources:
 			self.remote = remote
 			yield self.remote, 0, 0
 
