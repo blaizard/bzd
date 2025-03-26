9de8b8a46a8478180c71c168c42fa700ef4a5ce2
blaise <blaise.lengrand@gmail.com>
2025-03-26 07:38:03 +0100

    Adding cache2 (wip)

---
+++ nodejs/core/tests/cache2_test.mjs
@@ -0,0 +1,86 @@
+import Cache2 from "../cache2.mjs";
+import ExceptionFactory from "../exception.mjs";
+
+const Exception = ExceptionFactory("test", "cache2");
+
+describe("Cache2", () => {
+	describe("Simple", () => {
+		it("Base", async () => {
+			let cache = new Cache2();
+			let argRead = 0;
+			cache.register("test", (arg) => {
+				if (!arg) {
+					throw new Error("BOOM!");
+				}
+				argRead = arg;
+				return "return." + arg;
+			});
+			const result1 = await cache.get("test", "hello");
+			Exception.assertEqual(argRead, "hello");
+			Exception.assertEqual(result1, "return.hello");
+			const result2 = await cache.get("test", "world");
+			Exception.assertEqual(argRead, "world");
+			Exception.assertEqual(result2, "return.world");
+
+			await Exception.assertThrows(async () => {
+				await cache.get("tefst", "world");
+			});
+			await Exception.assertThrows(async () => {
+				await cache.get("test", false);
+			});
+			await Exception.assertThrows(async () => {
+				await cache.get("tedst");
+			});
+			await Exception.assertThrows(async () => {
+				await cache.get();
+			});
+		});
+	});
+
+	it("Stress", async () => {
+		let cache = new Cache2({
+			garbageCollector: false,
+			timeoutMs: 20 * 1000,
+		});
+
+		let sum = 0;
+		let expected = 0;
+		cache.register(
+			"test",
+			(arg) => {
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						resolve(parseInt(arg));
+					}, Math.random() * 10);
+				});
+			},
+			{
+				timeoutMs: 10,
+			},
+		);
+
+		const promiseDelay = (t) => {
+			return new Promise((resolve) => {
+				setTimeout(resolve, t);
+			});
+		};
+
+		let promises = [];
+		// eslint-disable-next-line
+		for (const i in [...Array(10000).keys()]) {
+			const n = Math.floor(Math.random() * 10);
+			expected += parseInt(n);
+			promises.push(
+				(async () => {
+					await promiseDelay(Math.random() * 10);
+					const result = await cache.get("test", n);
+					sum += result;
+				})(),
+			);
+		}
+
+		await Promise.all(promises);
+
+		Exception.assertEqual(sum, expected);
+	}).timeout(10000);
+});
