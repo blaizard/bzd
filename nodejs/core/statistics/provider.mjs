5cda920c212fe812f621e44899a78e335eae067a
blaise <blaise.lengrand@gmail.com>
2025-04-17 20:57:51 +0200

    statisctis (wip)

--- nodejs/core/statistics/provider.mjs
+++ nodejs/core/statistics/provider.mjs
@@ -2,5 +2,37 @@
 export default class Provider {
 	constructor(...namespace) {
 		this.namespace = namespace;
+		this.calls = {};
+		this.sizes = {};
+	}
+
+	/// Get the current timestamp in ms.
+	static _getTimestamp() {
+		return Date.now();
+	}
+
+	/// Time the given process.
+	async timeit(name, callback) {
+		const start = Provider._getTimestamp();
+		try {
+			await callback();
+		} finally {
+			this.calls[name] ??= {
+				count: 0,
+				duration: 0,
+			};
+			++this.calls[name].count;
+			this.calls[name].duration = Provider._getTimestamp() - start;
+		}
+	}
+
+	/// Register the given size.
+	setSize(name, size) {
+		this.sizes[name] ??= {
+			current: size,
+			max: size,
+		};
+		this.sizes[name].current = size;
+		this.sizes[name].max = Math.max(size, this.sizes[name].max);
 	}
 }
--- nodejs/core/statistics/statistics.mjs
+++ nodejs/core/statistics/statistics.mjs
@@ -6,11 +6,30 @@ const Log = LogFactory("statistics");
 
 /// This class is a statistics manager.
 export default class Statistics {
-	/// Register a startistic provider.
+	constructor() {
+		this.data = {};
+	}
+
+	/// Create the statistics UID from the name and the service provider.
+	_makeStartisticsUid(name, provider) {
+		if (name === null) {
+			let counter = 0;
+			do {
+				name = (++counter).toString();
+			} while (this._makeStartisticsUid(name, provider) in this.data);
+		}
+		return provider.namespace.join(".") + "." + name;
+	}
+
+	/// Register a statistic provider.
 	///
 	/// \param provider The statistics provider.
 	/// \param name The name of the statistics.
-	register(provider, name = null) {}
+	register(provider, name = null) {
+		const uid = this._makeStartisticsUid(name, provider);
+		Exception.assert(!(uid in this.data), "Statistics '{}' is already registered.", uid);
+		this.data[uid] = provider;
+	}
 
 	installRest(api) {
 		Log.info("Installing 'Statistics' REST.");
