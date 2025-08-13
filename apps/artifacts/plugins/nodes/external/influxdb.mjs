38ae21e9718df737cbc919d345a9da884b99f4ef
blaise <blaise.lengrand@gmail.com>
2025-08-13 20:57:26 +0200

    adding external source

--- apps/artifacts/plugins/nodes/external/influxdb.mjs
+++ apps/artifacts/plugins/nodes/external/influxdb.mjs
@@ -2,4 +2,4 @@ import ExceptionFactory from "#bzd/nodejs/core/exception.mjs";
 
 const Exception = ExceptionFactory("artifacts", "nodes", "external", "influxdb");
 
-export default async function external(uid, internal, count, after, before) {}
+export default async function external(uid, key, count, after, before) {}
