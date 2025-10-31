10a79ea8a0ca34b1b807ac52085e3ac65c9bc564
blaise <blaise.lengrand@gmail.com>
2025-10-31 17:10:53 +0900

    Adding docker-compose

--- apps/external/defs.bzl
+++ apps/external/defs.bzl
@@ -1,32 +1,35 @@
-"""Simple docker binary."""
+"""Docker binary based on docker-compose."""
 
 load("@bzd_lib//:sh_binary_wrapper.bzl", "sh_binary_wrapper_impl")
+load("@bzd_lib//config:defs.bzl", "bzd_config_default")
 
-def _bzd_docker_binary_impl(ctx):
+def _bzd_docker_json_binary_impl(ctx):
     args = []
     if ctx.attr.expose:
         args += ["--expose", str(ctx.attr.expose)]
-    args.append(ctx.attr.image)
+    args.append(ctx.label.name)
 
     return sh_binary_wrapper_impl(
         ctx = ctx,
         locations = {
             ctx.attr._docker: "docker",
+            ctx.file.docker_compose: "docker_compose",
         },
         output = ctx.outputs.executable,
-        command = "{docker} " + " ".join(args),
+        command = "{docker} " + " ".join(args) + " {docker_compose} \"$@\"",
     )
 
-bzd_docker_binary = rule(
+_bzd_docker_json_binary = rule(
     doc = "Docker binary.",
-    implementation = _bzd_docker_binary_impl,
+    implementation = _bzd_docker_json_binary_impl,
     attrs = {
+        "docker_compose": attr.label(
+            mandatory = True,
+            allow_single_file = True,
+            doc = "The name of the Docker image to use.",
+        ),
         "expose": attr.int(
             doc = "Port to expose.",
-        ),
-        "image": attr.string(
-            mandatory = True,
-            doc = "The name of the Docker image to use.",
         ),
         "_docker": attr.label(
             default = Label("//apps/external:docker"),
@@ -35,4 +38,32 @@ bzd_docker_binary = rule(
         ),
     },
     executable = True,
+)
+
+def _bzd_docker_binary_impl(name, visibility, docker_compose, **kwargs):
+    bzd_config_default(
+        name = "{}.config".format(name),
+        srcs = [
+            docker_compose,
+        ],
+    )
+
+    _bzd_docker_json_binary(
+        name = name,
+        visibility = visibility,
+        docker_compose = "{}.config.json".format(name),
+        **kwargs
+    )
+
+bzd_docker_binary = macro(
+    inherit_attrs = _bzd_docker_json_binary,
+    attrs = {
+        "docker_compose": attr.label(
+            allow_single_file = ["yaml"],
+            mandatory = True,
+            doc = "The docker-compose.yaml file associated with this instance.",
+            configurable = False,
+        ),
+    },
+    implementation = _bzd_docker_binary_impl,
 )
--- apps/external/docker.py
+++ apps/external/docker.py
@@ -1,18 +1,22 @@
 import argparse
 import typing
 import socket
+import pathlib
+import json
+import tempfile
 from contextlib import closing
 
-from bzd.utils.run import localDocker
+from bzd.utils.run import localCommand
 from bzd.utils.scheduler import Scheduler
 from bzd.http.client import HttpClient
+from bzd.utils.to_yaml import toYaml
 
 
 def findFreePort() -> int:
 	"""Find a free port on the local machine."""
 
 	with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-		s.bind(('localhost', 0))
+		s.bind(("localhost", 0))
 		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		return int(s.getsockname()[1])
 
@@ -30,25 +34,77 @@ def httpServerReady(port: int) -> bool:
 	return False
 
 
+class DockerCompose:
+	"""Modifier class for docker compose files."""
+
+	def __init__(self, path: pathlib.Path) -> None:
+		self.data = json.loads(path.read_text())
+
+	def _getService(self, service: str) -> typing.Dict[str, typing.Any]:
+		"""Get a specific service from the docker-compose schema."""
+
+		assert service in self.data[
+		    "services"], f"The service '{service}' is not present in the docker-compose.yaml file."
+		return self.data["services"][service]  # type: ignore
+
+	def prepare(self, service: str) -> None:
+		"""Prepare the service for being used."""
+
+		serviceData = self._getService(service)
+		# This tells Docker to use a lightweight init process as the container's main entrypoint.
+		# This special process is designed to do one thing well: receive signals (like Ctrl+C)
+		# and properly forward them to your application.
+		serviceData["init"] = True
+
+	def addExposedPort(self, service: str, local: int, docker: int) -> None:
+		"""Add an exposed port to a specific service."""
+
+		serviceData = self._getService(service)
+		serviceData.setdefault("ports", []).append(f"{local}:{docker}")
+
+	def toYaml(self) -> str:
+		"""Get the updated data of the docker-compose schema."""
+
+		return toYaml(self.data)
+
+
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description="Run a docker image.")
 	parser.add_argument("--expose", type=int, default=None, help="Port to expose.")
-	parser.add_argument("image", type=str, help="The docker image to run.")
+	parser.add_argument("-v", "--verbose", action="store_true", help="Display the output as it goes.")
+	parser.add_argument("service", type=str, help="The name of the service.")
+	parser.add_argument("docker_compose", type=pathlib.Path, help="The docker-compose file in json format.")
 	args = parser.parse_args()
 
 	exposedPort: typing.Optional[int] = None
+	dockerCompose = DockerCompose(args.docker_compose)
+	dockerCompose.prepare(args.service)
 
-	command = ["run", "--rm"]  #, "-it"]
 	if args.expose is not None:
 		exposedPort = findFreePort()
-		command += ["-p", f"{exposedPort}:{args.expose}"]
-	command += [args.image]
+		dockerCompose.addExposedPort(args.service, exposedPort, args.expose)
+
+	yaml = dockerCompose.toYaml()
 
 	scheduler = Scheduler(blocking=False)
 	if exposedPort is not None:
 		scheduler.add(name="ping", intervalS=0.5, callback=httpServerReady, args=(exposedPort, ))
 	scheduler.start()
 
-	print(f"Setting up application, please wait...")
-	localDocker(command, timeoutS=None)  #, stdin=True, stdout=True, stderr=True)
+	kwargs: typing.Dict[str, typing.Any] = {}
+	if args.verbose:
+		kwargs["stdout"] = True
+		kwargs["stderr"] = True
+
+	with tempfile.NamedTemporaryFile(delete_on_close=False) as f:
+		f.write(yaml.encode())
+		f.close()
+
+		print(f"Setting up service '{args.service}', please wait...")
+		try:
+			localCommand(["docker", "compose", "--file", f.name, "run", "--service-ports", "--rm", args.service],
+			             timeoutS=None,
+			             **kwargs)
+		finally:
+			localCommand(["docker", "compose", "--file", f.name, "down", "--remove-orphans"], ignoreFailure=True)
