4bbfa81dc5162b0ce3870af90b8ce284db640f20
blaise <blaise.lengrand@gmail.com>
2026-03-16 18:04:04 +0900

    Removed zellij

--- tools/shell/sh/bashrc.sh
+++ tools/shell/sh/bashrc.sh
@@ -188,13 +188,5 @@ bzd_update()
 	bzd_reload
 }
 
-# ---- Content from zellij.sh
-# Auto-start Zellij unless inside VS Code or already in a Zellij session
-if [[ "$TERM_PROGRAM" != "vscode" ]] && [[ -z "$ZELLIJ" ]]; then
-    if command -v zellij > /dev/null; then
-        exec zellij
-    fi
-fi
-
 
 fi
--- tools/shell/sh/srcs/interactive/zellij.sh
+++
@@ -1,6 +0,0 @@
-# Auto-start Zellij unless inside VS Code or already in a Zellij session
-if [[ "$TERM_PROGRAM" != "vscode" ]] && [[ -z "$ZELLIJ" ]]; then
-    if command -v zellij > /dev/null; then
-        exec zellij
-    fi
-fi
