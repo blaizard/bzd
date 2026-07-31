505c2dd53f90df749eaf522c597714ac4edf6a52
blaise <blaise.lengrand@gmail.com>
2026-07-31 17:32:25 +0900

    Refactored run.js

--- nodejs/utils/run.js
+++ nodejs/utils/run.js
@@ -30,6 +30,7 @@ class ExecuteResult {
 
 	async join() {
 		await this.promise;
+		return this;
 	}
 
 	_addToBuffer(isStdout, data) {
@@ -112,81 +113,97 @@ class ExecuteResult {
 	}
 }
 
-export async function localCommand(cmds, { stdout = null, stderr = null, maxOutputSize = 1000000 } = {}) {
+export function localCommand(
+	cmds,
+	{ stdout = null, stderr = null, maxOutputSize = 1000000, ignoreFailure = false, env = null } = {},
+) {
 	Exception.assert(Array.isArray(cmds) && cmds.length > 0, "No command to run");
 
-	const subprocess = spawn(cmds[0], cmds.slice(1), {
+	const spawnOptions = {
 		stdio: ["pipe", "pipe", "pipe"],
-	});
+	};
+	if (env !== null) {
+		spawnOptions.env = env;
+	}
+
+	const subprocess = spawn(cmds[0], cmds.slice(1), spawnOptions);
 	const result = new ExecuteResult(subprocess, { maxOutputSize: maxOutputSize });
 
-	return new Promise((resolve, reject) => {
-		// Helper for the output handlers.
-		if (stdout === null) {
-			stdout = [];
-		}
-		if (stderr === null) {
-			stderr = [];
-		}
-		if (stdout === true) {
-			stdout = [process.stdout.write];
-		}
-		if (stderr === true) {
-			stderr = [process.stderr.write];
-		}
-		if (typeof stdout === "function") {
-			stdout = [stdout];
-		}
-		if (typeof stderr === "function") {
-			stderr = [stderr];
-		}
-		Exception.assert(Array.isArray(stdout), "Invalid stdout handlers");
-		Exception.assert(Array.isArray(stderr), "Invalid stderr handlers");
-		if (maxOutputSize > 0) {
-			stdout.push((data) => result._addToBuffer(true, data));
-			stderr.push((data) => result._addToBuffer(false, data));
-		}
+	// Helper for the output handlers.
+	if (stdout === null) {
+		stdout = [];
+	}
+	if (stderr === null) {
+		stderr = [];
+	}
+	if (stdout === true) {
+		stdout = [process.stdout.write];
+	}
+	if (stderr === true) {
+		stderr = [process.stderr.write];
+	}
+	if (typeof stdout === "function") {
+		stdout = [stdout];
+	}
+	if (typeof stderr === "function") {
+		stderr = [stderr];
+	}
+	Exception.assert(Array.isArray(stdout), "Invalid stdout handlers");
+	Exception.assert(Array.isArray(stderr), "Invalid stderr handlers");
+	if (maxOutputSize > 0) {
+		stdout.push((data) => result._addToBuffer(true, data));
+		stderr.push((data) => result._addToBuffer(false, data));
+	}
 
-		subprocess.stdout.setEncoding("utf8");
-		subprocess.stdout.on("data", (data) => {
-			stdout.forEach((handler) => handler(data));
-		});
-		subprocess.stderr.setEncoding("utf8");
-		subprocess.stderr.on("data", (data) => {
-			stderr.forEach((handler) => handler(data));
-		});
+	subprocess.stdout.setEncoding("utf8");
+	subprocess.stdout.on("data", (data) => {
+		stdout.forEach((handler) => handler(data));
+	});
+	subprocess.stderr.setEncoding("utf8");
+	subprocess.stderr.on("data", (data) => {
+		stderr.forEach((handler) => handler(data));
+	});
 
-		// Do not reject not to end up with an unhandled rejection.
-		result.promise = new Promise((finalResolve, _) => {
-			subprocess.on("close", (code) => {
-				if (typeof code === "number") {
-					result.returncode = code;
-					if (code == 0) {
-						result.event.trigger("status", Status.completed);
-						finalResolve();
-					} else {
-						result.event.trigger("status", Status.failed);
-						const message = "Process failed with error code: " + code;
-						stderr.forEach((handler) => handler(message));
-						finalResolve();
-					}
-				} else {
-					result.event.trigger("status", Status.cancelled);
-					stderr.forEach((handler) => handler("Cancelled"));
-					finalResolve();
-				}
-			});
-		});
+	subprocess.on("spawn", () => {
+		result.event.trigger("status", Status.running);
+	});
 
-		subprocess.on("spawn", () => {
-			result.event.trigger("status", Status.running);
-			resolve(result);
-		});
+	// Do not reject not to end up with an unhandled rejection.
+	result.promise = new Promise((resolve, reject) => {
+		const handleError = () => {
+			if (ignoreFailure) {
+				resolve();
+			} else {
+				reject(new Exception("While executing: {}\n{}", cmds.join(" "), result.getOutput()));
+			}
+		};
+
 		subprocess.on("error", (err) => {
 			result.event.trigger("status", Status.failed);
 			const message = "Process failed to start: " + String(err);
 			stderr.forEach((handler) => handler(message));
-			reject(new Exception(message));
+			handleError();
+		});
+
+		subprocess.on("close", (code) => {
+			if (typeof code === "number") {
+				result.returncode = code;
+				if (code == 0) {
+					result.event.trigger("status", Status.completed);
+					resolve();
+				} else {
+					result.event.trigger("status", Status.failed);
+					const message = "Process failed with error code: " + code;
+					stderr.forEach((handler) => handler(message));
+					handleError();
+				}
+			} else {
+				result.event.trigger("status", Status.cancelled);
+				stderr.forEach((handler) => handler("Cancelled"));
+				handleError();
+			}
 		});
 	});
+
+	return result;
 }
