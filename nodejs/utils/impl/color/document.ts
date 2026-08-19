7ed332d3cbb9f2fd898f2fdefedf71c098264baa
blaise <blaise.lengrand@gmail.com>
2026-08-19 17:57:06 +0900

    Fixed missing types

--- nodejs/utils/impl/color/document.ts
+++ nodejs/utils/impl/color/document.ts
@@ -1,9 +1,13 @@
+import ExceptionFactory from "#bzd/nodejs/core/exception.js";
+
+const Exception = ExceptionFactory("color");
+
 export default {
 	normalize: (color: string): string => {
         const context = document.createElement("canvas").getContext("2d");
         Exception.assert(context, "Unable to create a 2D canvas context");
         const ctx = context as CanvasRenderingContext2D;
-        ctx.fillStyle = str;
+        ctx.fillStyle = color;
         return ctx.fillStyle;
     }
 }
