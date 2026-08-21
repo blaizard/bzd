8e8b1020022e177e0a9000fb28a8bad02f887a8d
blaise <blaise.lengrand@gmail.com>
2026-08-22 07:10:00 +0900

    uid.js -> ts

--- nodejs/utils/uid.js
+++
@@ -1,5 +0,0 @@
-/// Generate a unique ID
-let _uidCounter = 0;
-export function makeUid() {
-	return "uid-" + Date.now() + "-" + Math.random().toString(36).substr(2, 8) + "-" + ++_uidCounter;
-}
---
+++ nodejs/utils/uid.ts
@@ -0,0 +1,5 @@
+/// Generate a unique ID
+let _uidCounter = 0;
+export function makeUid(): string {
+    return "uid-" + Date.now() + "-" + Math.random().toString(36).substr(2, 8) + "-" + ++_uidCounter;
+}
