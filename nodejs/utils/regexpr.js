abda586553c87972667a32cd7e41c97a8079f371
blaise <blaise.lengrand@gmail.com>
2026-08-22 06:49:13 +0900

    regexpr.js -> ts

--- nodejs/utils/regexpr.js
+++
@@ -1,4 +0,0 @@
-/// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-export function regexprEscape(string) {
-	return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
-}
