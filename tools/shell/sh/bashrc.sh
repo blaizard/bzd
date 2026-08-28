f2a8c77f8774095bfbeef499e3d61edf88617d36
blaise <blaise.lengrand@gmail.com>
2026-08-28 17:27:20 +0900

    Don't delete .bzd file

--- tools/shell/sh/bashrc.sh
+++ tools/shell/sh/bashrc.sh
@@ -328,7 +328,7 @@ fi
 bzd_update()
 {
 	bzd_fetch "https://raw.githubusercontent.com/blaizard/cpp-async/master/tools/shell/sh/bashrc.sh" ~/.bzd_update_temp
-	rm -rfd ~/.bzd && mkdir -p ~/.bzd && mv ~/.bzd_update_temp ~/.bzd/bashrc.sh
+	rm -rfd ~/.bzd/bin && mkdir -p ~/.bzd && mv ~/.bzd_update_temp ~/.bzd/bashrc.sh
 	bzd_reload
 }
 
