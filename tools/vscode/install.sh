2ba9eaf59ce5bf375d41bb7dddbcb1eafd921f1f
blaise <blaise.lengrand@gmail.com>
2026-09-17 17:45:19 +0900

    Make vscode extension pointing symlink

--- tools/vscode/install.sh
+++ tools/vscode/install.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 VSCODE_EXTENSION="$HOME/.vscode/extensions"
 
-rm -rfd "${VSCODE_EXTENSION}/bdl"
-cp -r "${SCRIPTPATH}/bdl" "${VSCODE_EXTENSION}"
+rm -rfd "${VSCODE_EXTENSION}/bzd"
+ln -sfn "${SCRIPTPATH}/bzd" "${VSCODE_EXTENSION}/bzd"
 
 # https://stackoverflow.com/questions/42116486/where-can-i-find-a-list-of-all-possible-keys-for-tm-themes-for-syntax-highlighti
